97b2e46894db06f4d34c7d4ce030de6a67b2e24d
Jeremy Zheng <jitang.zheng@gmail.com>
2021-10-28 09:39:46 -0700

    :package: split grpc install script

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -2,41 +2,7 @@
 
 set -e
 
-# https://grpc.io/docs/languages/cpp/quickstart/
-function build_grpc() {
-    if [ -L $HOME/.local/bin/protoc ]
-    then
-        return
-    fi
-    if [ -d $HOME/downloads/grpc ]
-    then
-        cd $HOME/downloads/grpc
-        git checkout $1
-        git submodule update --init --recursive
-    else
-        git clone --recurse-submodules -b $1 https://github.com/grpc/grpc.git $HOME/downloads/grpc
-    fi
-    
-    cd $HOME/downloads/grpc/third_party/protobuf
-    git checkout $2
-    # fix build for glibc 2.34: https://github.com/abseil/abseil-cpp/issues/952
-    cd $HOME/downloads/grpc/third_party/abseil-cpp
-    git checkout 20210324.2
 
-    if [ -d $HOME/build/grpc-amd64 ]
-    then
-        rm -r $HOME/build/grpc-amd64
-    fi
-    mkdir -pv $HOME/build/grpc-amd64
-    cd $HOME/build/grpc-amd64
-    cmake -DCMAKE_BUILD_TYPE=Release \
-    -DgRPC_INSTALL=ON \
-    -DgRPC_SSL_PROVIDER=package \
-    -DgRPC_BUILD_TESTS=OFF \
-    -DCMAKE_INSTALL_PREFIX=$HOME/.local $HOME/downloads/grpc
-    make
-    make install
-}
 
 
 declare -a languages=(
@@ -50,8 +16,6 @@ declare -a languages=(
 
 export WORKSPACE=$PWD
 export PROTOBUF_ROOT=$HOME/.local
-
-build_grpc "v1.39.1" "v3.17.1"
 
 for l in "${languages[@]}"
 do
