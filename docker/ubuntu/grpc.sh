b70f1346e4491e9add2bbfef7c1d40d8c401ef5e
Jeremy Zheng <jitang.zheng@gmail.com>
2021-10-29 13:27:05 +0000

    :package: add clang docker

--- docker/ubuntu/grpc.sh
+++ docker/ubuntu/grpc.sh
@@ -2,43 +2,45 @@
 
 set -e
 
+if [ "$#" -ne 2 ]
+then
+    echo "USAGE: $0 GRPC_VERSION PROTOBUF_VERSION"
+    exit 1
+fi
+
 # https://grpc.io/docs/languages/cpp/quickstart/
-function build_grpc() {
-    if [ -L $HOME/.local/bin/protoc ]
-    then
-        return
-    fi
-    if [ -d $HOME/downloads/grpc ]
-    then
-        cd $HOME/downloads/grpc
-        git checkout $1
-        git submodule update --init --recursive
-    else
-        git clone --recurse-submodules -b $1 https://github.com/grpc/grpc.git $HOME/downloads/grpc
-    fi
-    
-    cd $HOME/downloads/grpc/third_party/protobuf
-    git checkout $2
-    # fix build for glibc 2.34: https://github.com/abseil/abseil-cpp/issues/952
-    cd $HOME/downloads/grpc/third_party/abseil-cpp
-    git checkout 20210324.2
+if [ -L $HOME/.local/bin/protoc ]
+then
+    return
+fi
+if [ -d $HOME/downloads/grpc ]
+then
+    cd $HOME/downloads/grpc
+    git checkout $1
+    git submodule update --init --recursive
+else
+    git clone --recurse-submodules -b $1 https://github.com/grpc/grpc.git $HOME/downloads/grpc
+fi
 
-    if [ -d $HOME/build/grpc-amd64 ]
-    then
-        rm -r $HOME/build/grpc-amd64
-    fi
-    mkdir -pv $HOME/build/grpc-amd64
-    cd $HOME/build/grpc-amd64
-    cmake -DCMAKE_BUILD_TYPE=Release \
-    -DgRPC_INSTALL=ON \
-    -DgRPC_SSL_PROVIDER=package \
-    -DgRPC_BUILD_TESTS=OFF \
-    -DCMAKE_INSTALL_PREFIX=$HOME/.local $HOME/downloads/grpc
-    make
-    make install
-}
+cd $HOME/downloads/grpc/third_party/protobuf
+git checkout $2
+# fix build for glibc 2.34: https://github.com/abseil/abseil-cpp/issues/952
+cd $HOME/downloads/grpc/third_party/abseil-cpp
+git checkout 20210324.2
 
-build_grpc "v1.39.1" "v3.17.1"
+if [ -d $HOME/build/grpc-amd64 ]
+then
+    rm -r $HOME/build/grpc-amd64
+fi
+mkdir -pv $HOME/build/grpc-amd64
+cd $HOME/build/grpc-amd64
+cmake -DCMAKE_BUILD_TYPE=Release \
+-DgRPC_INSTALL=ON \
+-DgRPC_SSL_PROVIDER=package \
+-DgRPC_BUILD_TESTS=OFF \
+-DCMAKE_INSTALL_PREFIX=$HOME/.local $HOME/downloads/grpc
+make
+make install
 
 echo 'done.'
 
