5b693abb2f9931986638c96bdd3dc3eeb8f0b76f
Jeremy Zheng <jitang.zheng@gmail.com>
2021-10-29 10:36:28 -0700

    :package: mv grpc.sh to scripts

--- docker/ubuntu/conan/install.sh
+++ docker/ubuntu/conan/install.sh
@@ -2,24 +2,22 @@
 
 set -e
 
-
-
 export OS_NAME=$(lsb_release -is)
 
 if [[ $OS_NAME == "Ubuntu" ]]
 then
     declare -a profiles=(
-        "amd64-linux"
-        "armhf-linux"
-        "arm64-linux"
+        "amd64-clang"
+        "armhf-clang"
+        "arm64-clang"
     )
     for p in "${profiles[@]}"
     do
-        conan install --build=missing --profile:build=default --profile:host=$p .
+        conan install --build --profile:build=default --profile:host=./profiles/$p .
     done    
 elif [[ $OS_NAME == "Arch" ]]
 then
-    conan install --build=missing --profile:build=default --profile:host=archlinux .
+    conan install --build --profile:build=default --profile:host=./profiles/arch-clang .
 else
     echo "Unknowk os $OS_NAME"
     exit 1
