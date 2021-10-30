a34b1da625e36e43353070b3240cd115699e037e
Jeremy Zheng <jitang.zheng@gmail.com>
2021-10-30 00:30:09 -0700

    :package: add clang image

---
+++ docker/clang/conan/toolchains/arm64.cmake
@@ -0,0 +1,18 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(target aarch64-none-linux-gnu)
+set(clang_home $ENV{HOME}/local/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04)
+set(gcc_home $ENV{HOME}/local/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu)
+
+set(CMAKE_C_COMPILER ${clang_home}/bin/clang)
+set(CMAKE_C_COMPILER_TARGET ${target})
+set(CMAKE_CXX_COMPILER ${clang_home}/bin/clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${target})
+set(CMAKE_C_FLAGS "-ccc-gcc-name ${gcc_home}/bin/${target}-gcc")
+set(CMAKE_CXX_FLAGS "-stdlib=libstdc++ -ccc-gcc-name ${gcc_home}/bin/${target}-gcc")
+set(CMAKE_EXE_LINKER_FLAGS "--ld-path=${clang_home}/bin/ld.lld")
+
+set(CMAKE_FIND_ROOT_PATH ${gcc_home}/${target}/libc)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
