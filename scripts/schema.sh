acc11d13191c2ee1163d4fbc91b60ef9effcb370
Jeremy Zheng <jitang.zheng@gmail.com>
2021-12-06 10:19:43 -0800

    :construction: merge protocols

---
+++ scripts/schema.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# ---------------------------------------------------------
+
+declare -a languages=(
+    # "node"
+    "php"
+    "python"
+    "ruby"
+    "cpp"
+    "csharp"
+    # "objective_c"
+)
+
+
+export WORKSPACE=$PWD
+export PROTOBUF_ROOT=$HOME/.local
+
+for l in "${languages[@]}"
+do
+    if [ -d $WORKSPACE/protocols/$l ]
+    then
+        rm -r $WORKSPACE/protocols/$l
+    fi
+    mkdir -pv $WORKSPACE/protocols/$l
+    $PROTOBUF_ROOT/bin/protoc -I $WORKSPACE/protos \
+        -I $PROTOBUF_ROOT/include/google/protobuf \
+        --${l}_out=$WORKSPACE/protocols/$l --grpc_out=$WORKSPACE/protocols/$l \
+        --plugin=protoc-gen-grpc=$PROTOBUF_ROOT/bin/grpc_${l}_plugin \
+        $WORKSPACE/protos/*.proto
+done
+
+# ---------------------------------------------------------
+
+echo 'done.'
+exit 0
