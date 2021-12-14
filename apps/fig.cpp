eb697b2220ef847d782295299bae5d474c02d2bd
Jeremy Zheng <jitang.zheng@gmail.com>
2021-12-13 17:04:20 -0800

    :construction: add application options

--- apps/fig.cpp
+++ apps/fig.cpp
@@ -1,16 +1,3 @@
 #include "palm/fig.hpp"
-#include "palm/version.hpp"
 
-#include <cstdlib>
-
-#include <boost/log/trivial.hpp>
-
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
-#include <grpcpp/grpcpp.h>
-#include <grpcpp/health_check_service_interface.h>
-
-int main() {
-  BOOST_LOG_TRIVIAL(info) << "start " << PALM_GIT_VERSION << "("
-                          << PALM_BUILD_TIME << ")";
-  return EXIT_SUCCESS;
-}
+POCO_APP_MAIN(palm::fig::Application)
--- include/palm/fig.hpp
+++ include/palm/fig.hpp
@@ -12,7 +12,7 @@ class Rpc : public Poco::Util::Subsystem {
   const char* name() const override { return "rpc"; }
   void defineOptions(Poco::Util::OptionSet& options) override;
   void initialize(Poco::Util::Application& app) override;
-  void uninitialize() override;
+  void uninitialize() override {}
 
  private:
 };
@@ -22,7 +22,7 @@ class Web : public Poco::Util::Subsystem {
   const char* name() const override { return "web"; }
   void defineOptions(Poco::Util::OptionSet& options) override;
   void initialize(Poco::Util::Application& app) override;
-  void uninitialize() override;
+  void uninitialize() override {}
 
  private:
 };
@@ -32,14 +32,30 @@ class Worker : public Poco::Util::Subsystem {
   const char* name() const override { return "worker"; }
   void defineOptions(Poco::Util::OptionSet& options) override;
   void initialize(Poco::Util::Application& app) override;
-  void uninitialize() override;
+  void uninitialize() override {}
 
  private:
 };
 
 class Application : public Poco::Util::Application {
  public:
+  Application()
+      : Poco::Util::Application(),
+        stopped(false),
+        config_file("config.properties") {}
+  void defineOptions(Poco::Util::OptionSet& options) override;
+  void initialize(Poco::Util::Application& app) override;
+  void uninitialize() override {}
+  int main(const std::vector<std::string>& args) override;
+
  private:
+  void handleDebug(const std::string& name, const std::string& value);
+  void handleHelp(const std::string& name, const std::string& value);
+  void handleConfig(const std::string& name, const std::string& value);
+  void handleVersion(const std::string& name, const std::string& value);
+
+  bool stopped;
+  std::filesystem::path config_file;
 };
 }  // namespace fig
 }  // namespace palm
