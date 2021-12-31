498850ad4b704918502f235de035e2781ec3c018
Jeremy Zheng <jitang.zheng@gmail.com>
2021-12-30 18:05:31 -0800

    :construction: add grpc-web support

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -20,17 +20,31 @@ export PROTOBUF_ROOT=$HOME/.local
 
 for l in "${languages[@]}"
 do
+    echo "generate code for $l"
     if [ -d $WORKSPACE/protocols/$l ]
     then
         rm -r $WORKSPACE/protocols/$l
     fi
-    mkdir -pv $WORKSPACE/protocols/$l
+    mkdir -p $WORKSPACE/protocols/$l
     $PROTOBUF_ROOT/bin/protoc -I $WORKSPACE/protos \
         -I $PROTOBUF_ROOT/include/google/protobuf \
         --${l}_out=$WORKSPACE/protocols/$l --grpc_out=$WORKSPACE/protocols/$l \
         --plugin=protoc-gen-grpc=$PROTOBUF_ROOT/bin/grpc_${l}_plugin \
         $WORKSPACE/protos/*.proto
 done
+
+echo "generate code for grpc-web"
+export JS_OUT=$WORKSPACE/dashboard/src/protocols
+if [ -d $JS_OUT ]
+then
+    rm -r $JS_OUT
+fi
+mkdir -p $JS_OUT
+$PROTOBUF_ROOT/bin/protoc -I $WORKSPACE/protos \
+    -I $PROTOBUF_ROOT/include/google/protobuf \
+    --js_out=import_style=commonjs:$JS_OUT \
+    --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$JS_OUT \
+    $WORKSPACE/protos/*.proto
 
 # ---------------------------------------------------------
 
