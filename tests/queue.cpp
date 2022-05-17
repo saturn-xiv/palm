6af1c406ef7c437f8bb3c5fd9ff2e180a4210af8
Jeremy Zheng <jitang.zheng@gmail.com>
2022-05-17 16:04:34 -0700

    :construction: add rabbitmq support

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -1,8 +1,55 @@
 #define BOOST_TEST_MODULE queue
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(zeromq) { BOOST_TEST(4 == 2 * 2); }
+#include "palm/queue.hpp"
 
-BOOST_AUTO_TEST_CASE(rabbitmq) { BOOST_TEST(4 == 2 * 2); }
+#include <iostream>
 
-BOOST_AUTO_TEST_CASE(mqtt) { BOOST_TEST(4 == 2 * 2); }
+BOOST_AUTO_TEST_CASE(amqp_pub) {
+  auto& mq = palm::rabbitmq::Factory::instance();
+  {
+    boost::property_tree::ptree cfg;
+    cfg.put("rabbitmq.virtual-host", "/demo");
+    mq.set(cfg);
+  }
+  auto con = mq.create();
+  for (int i = 0; i < 10; i++) {
+    std::cout << "publish message " << i << std::endl;
+    std::stringstream ss;
+
+    ss << "Hello, palm!(" << i << ")";
+    con->publish("hi", "text/plain", ss.str());
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+class EchoHandler : public palm::rabbitmq::Handler {
+ public:
+  void execute(const std::string& id, const std::string& content_type,
+               const std::string& payload) override {
+    std::cout << "receive message: " << id << " " << content_type << " "
+              << payload << std::endl;
+  }
+};
+
+BOOST_AUTO_TEST_CASE(amqp_sub) {
+  auto& mq = palm::rabbitmq::Factory::instance();
+  {
+    boost::property_tree::ptree cfg;
+    cfg.put("rabbitmq.virtual-host", "/demo");
+    mq.set(cfg);
+  }
+  auto con = mq.create();
+  EchoHandler handler;
+  for (;;) {
+    con->consume("hi", handler);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(zeromq) {
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(mqtt) {
+  // TODO
+}
