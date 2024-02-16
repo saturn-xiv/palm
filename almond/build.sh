eedc1849789bcf8bf5e730e0a0361551dbf695c6
Jeremy Zheng <jitang.zheng@gmail.com>
2024-02-16 08:04:47 +0000

    :hammer: add build script

---
+++ almond/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+export WORKSPACE=$PWD
+export GIT_VERSION=$(git describe --tags --always --dirty --first-parent)
+export PACKAGE_NAME=almond-$GIT_VERSION
+export TARGET=$WORKSPACE/tmp/$PACKAGE_NAME
+
+# ---------------------------------------------------------
+
+function build_backend() {
+    cd $WORKSPACE
+
+    local pkg="github.com/saturn-xiv/palm/almond/cmd"
+    local ldflags="-s -w -X '$pkg.repo_url=$(git remote get-url origin)' -X '$pkg.author_name=$(git config --get user.name)' -X '$pkg.author_email=$(git config --get user.email)' -X '$pkg.build_time=$(date -u)' -X '$pkg.git_version=$(git describe --tags --always --dirty --first-parent)'"
+
+    echo "build for $2"
+    GOOS=linux GOARCH=$1 go build -ldflags "$ldflags"
+    mkdir -p $TARGET/bin/$2
+    mv almond $TARGET/bin/$2/
+}
+
+# ---------------------------------------------------------
+
+build_backend amd64 x86_64
+build_backend arm64 aarch64
+build_backend riscv64 riscv64
+
+cd $WORKSPACE/tmp
+echo "compressing $PACKAGE_NAME..."
+XZ_OPT=-9 tar -cJf $PACKAGE_NAME.tar.xz $PACKAGE_NAME
+md5sum $PACKAGE_NAME.tar.xz >$PACKAGE_NAME.txt
+
+echo "done($PACKAGE_NAME)."
+
+exit 0
