903b71815065d1d987db41292e776c823b2c70fe
Jeremy Zheng <jitang.zheng@gmail.com>
2024-02-18 14:36:18 +0000

    :construction: add twilio callbacks

--- cactus/cmd/mod.go
+++ cactus/cmd/mod.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -16,9 +17,9 @@ var (
 )
 
 var root_cmd = &cobra.Command{
-	Use:     "petunia",
-	Short:   "Petunia",
-	Long:    fmt.Sprintf("A email worker by Go.(%s).", repo_url),
+	Use:     "cactus",
+	Short:   "Cactus",
+	Long:    fmt.Sprintf("A sms worker by Go.(%s).", repo_url),
 	Version: fmt.Sprintf("%s(%s) by %s<%s>", git_version, build_time, author_name, author_email),
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := cmd.Help(); err != nil {
@@ -37,12 +38,34 @@ var (
 	gl_debug            bool
 	gl_config           string
 	gl_send_email_queue string
+	gl_web_port         int
 )
 
 func init() {
 	root_cmd.PersistentFlags().BoolVarP(&gl_debug, "debug", "d", false, "run on debug mode")
 	root_cmd.PersistentFlags().StringVarP(&gl_config, "config", "c", "config.toml", "load configuration file")
 
+	{
+		var cmd = &cobra.Command{
+			Use:   "web",
+			Short: "Start a HTTP server",
+			Run: func(cmd *cobra.Command, args []string) {
+				if gl_debug {
+					log.SetLevel(log.DebugLevel)
+				} else {
+					log.SetLevel(log.InfoLevel)
+					gin.SetMode(gin.ReleaseMode)
+				}
+				log.Debugf("run on debug mode")
+
+				if err := launch_web_server(gl_web_port, gl_config); err != nil {
+					log.Fatalf("start HTTP server: %s", err)
+				}
+			},
+		}
+		cmd.Flags().IntVarP(&gl_web_port, "port", "p", 8080, "listen port")
+		root_cmd.AddCommand(cmd)
+	}
 	{
 		var cmd = &cobra.Command{
 			Use:   "send-sms",
---
+++ cactus/cmd/web.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+
+	twilio_controllers "github.com/saturn-xiv/palm/cactus/controllers/twilio"
+	"github.com/saturn-xiv/palm/cactus/env"
+)
+
+func launch_web_server(port int, config_file string) error {
+	log.Debugf("load configuration from %s", config_file)
+	config := env.Config{}
+	if _, err := toml.DecodeFile(config_file, &config); err != nil {
+		return err
+	}
+
+	address := fmt.Sprintf("127.0.0.1:%d", port)
+	router := gin.Default()
+
+	twilio_router := router.Group("/twilio")
+	twilio_controllers.Register(twilio_router)
+
+	log.Infof("listen on http://%s", address)
+	return router.Run(address)
+}
