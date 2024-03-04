c0004b6019bc0863212824f34d71953dad389b4c
Jeremy Zheng <jitang.zheng@gmail.com>
2024-03-04 20:20:06 +0800

    :card_file_box: add daffodil tables

---
+++ db/migrations/20240304113926_create-daffodil-ledgers.sql
@@ -0,0 +1,37 @@
+-- migrate:up
+CREATE TABLE daffodil_ledgers(
+    id SERIAL PRIMARY KEY,
+    owner_id INT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    summary VARCHAR(511) NOT NULL,
+    deleted_at TIMESTAMP WITHOUT TIME ZONE,
+    "version" INT NOT NULL DEFAULT 0,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE UNIQUE INDEX idx_daffodil_ledgers_owner_name ON daffodil_ledgers(owner_id, "name");
+CREATE INDEX idx_daffodil_ledgers_name ON daffodil_ledgers("name");
+CREATE TABLE daffodil_bills(
+    id SERIAL PRIMARY KEY,
+    author_id INT NOT NULL,
+    ledger_id INT NOT NULL,
+    summary VARCHAR(511) NOT NULL,
+    total MONEY NOT NULL,
+    currency CHAR(3) NOT NULL,
+    merchant VARCHAR(64) NOT NULL,
+    category VARCHAR(32) NOT NULL,
+    paid_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    paid_by VARCHAR(32) NOT NULL,
+    deleted_at TIMESTAMP WITHOUT TIME ZONE,
+    "version" INT NOT NULL DEFAULT 0,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_daffodil_bills_currency ON daffodil_bills(currency);
+CREATE INDEX idx_daffodil_bills_merchant ON daffodil_bills(merchant);
+CREATE INDEX idx_daffodil_bills_summary ON daffodil_bills(summary);
+CREATE INDEX idx_daffodil_bills_category ON daffodil_bills(category);
+CREATE INDEX idx_daffodil_bills_paid_by ON daffodil_bills(paid_by);
+-- migrate:down
+DROP TABLE daffodil_bills;
+DROP TABLE daffodil_ledgers;
--- scripts/schema.sh
+++ scripts/schema.sh
@@ -4,6 +4,7 @@ set -e
 
 export PROTOBUF_ROOT=$HOME/.local
 export WORKSPACE=$PWD
+export PALM_PROTOCOLS=$WORKSPACE/palm/protocols
 
 # -----------------------------------------------------------------------------
 
@@ -120,7 +121,6 @@ export WORKSPACE=$PWD
 
 # -----------------------------------------------------------------------------
 
-# generate_diesel_postgresql "postgres://www:change-me@127.0.0.1:5432/demo"
 # DATABASE_URL=tmp/db diesel print-schema >ops/router/src/schema.rs
 
 # declare -a languages=(
@@ -156,91 +156,113 @@ function generate_musa() {
     if [ -d $musa_target ]; then
         rm -r $musa_target
     fi
-    $PROTOBUF_ROOT/bin/protoc -I $WORKSPACE/musa \
+    $PROTOBUF_ROOT/bin/protoc -I $PALM_PROTOCOLS \
         -I $PROTOBUF_ROOT/include/google/protobuf \
         --java_out=$target --grpc_out=$target \
         --plugin=protoc-gen-grpc=$PROTOBUF_ROOT/bin/grpc_java_plugin \
-        $WORKSPACE/musa/musa.proto
-
+        $PALM_PROTOCOLS/musa.proto
 }
 
-function generate_lily() {
-    echo "generate gRPC for lily"
-    local target=$WORKSPACE/lily/pumpkin
-    if [ -d $target ]; then
-        rm -r $target
-    fi
-    mkdir $target
-    touch $target/__init__.py
+# function generate_lily() {
+#     echo "generate gRPC for lily"
+#     local target=$WORKSPACE/lily/pumpkin
+#     if [ -d $target ]; then
+#         rm -r $target
+#     fi
+#     mkdir $target
+#     touch $target/__init__.py
 
-    $PROTOBUF_ROOT/bin/protoc -I $WORKSPACE/lily \
-        -I $PROTOBUF_ROOT/include/google/protobuf \
-        --python_out=$target --grpc_out=$target \
-        --plugin=protoc-gen-grpc=$PROTOBUF_ROOT/bin/grpc_python_plugin \
-        $WORKSPACE/lily/lily.proto
-    sed -i 's/import lily_/from . import lily_/g' $target/lily_pb2_grpc.py
+#     $PROTOBUF_ROOT/bin/protoc -I $WORKSPACE/lily \
+#         -I $PROTOBUF_ROOT/include/google/protobuf \
+#         --python_out=$target --grpc_out=$target \
+#         --plugin=protoc-gen-grpc=$PROTOBUF_ROOT/bin/grpc_python_plugin \
+#         $WORKSPACE/lily/lily.proto
+#     sed -i 's/import lily_/from . import lily_/g' $target/lily_pb2_grpc.py
 
-    # echo "generate thrift for lily"
-    # local target=$WORKSPACE/tmp/lily-gen-python
-    # cd $WORKSPACE/tmp
-    # if [ -d $target ]; then
-    #     rm -r $target
-    # fi
-    # mkdir $target
-    # thrift -r -out $target --gen py $WORKSPACE/lily/lily.thrift
+#     # echo "generate thrift for lily"
+#     # local target=$WORKSPACE/tmp/lily-gen-python
+#     # cd $WORKSPACE/tmp
+#     # if [ -d $target ]; then
+#     #     rm -r $target
+#     # fi
+#     # mkdir $target
+#     # thrift -r -out $target --gen py $WORKSPACE/lily/lily.thrift
 
-    # local gourd_dir=$WORKSPACE/lily/gourd
-    # if [ -d $gourd_dir ]; then
-    #     rm -r $gourd_dir
-    # fi
-    # mv $target/lily/gourd $WORKSPACE/lily/gourd
-}
+#     # local gourd_dir=$WORKSPACE/lily/gourd
+#     # if [ -d $gourd_dir ]; then
+#     #     rm -r $gourd_dir
+#     # fi
+#     # mv $target/lily/gourd $WORKSPACE/lily/gourd
+# }
 
-function generate_go() {
-    local protocols_dir=$WORKSPACE/$1/protocols
-    local target=$WORKSPACE/$1/$2/v2
+# function generate_go() {
+#     local protocols_dir=$WORKSPACE/$1/protocols
+#     local target=$WORKSPACE/$1/$2/v2
 
-    echo "generate $2 for $1"
-    mkdir -p $target
-    protoc -I $protocols_dir -I $PROTOBUF_ROOT/include/google/protobuf \
-        --go_out=$target --go_opt=paths=source_relative \
-        --go-grpc_out=$target --go-grpc_opt=paths=source_relative \
-        $protocols_dir/$2.proto
-}
+#     echo "generate $2 for $1"
+#     mkdir -p $target
+#     protoc -I $protocols_dir -I $PROTOBUF_ROOT/include/google/protobuf \
+#         --go_out=$target --go_opt=paths=source_relative \
+#         --go-grpc_out=$target --go-grpc_opt=paths=source_relative \
+#         $protocols_dir/$2.proto
+# }
 
 function generate_morus() {
     echo "generate code for morus"
-    local protocols_dir=$WORKSPACE/morus/protocols
     local target=$WORKSPACE/morus/src/protocols
 
     if [ -d $target ]; then
         rm -r $target
     fi
     mkdir -p $target
-    grpc_tools_node_protoc -I $protocols_dir \
+    grpc_tools_node_protoc -I $PALM_PROTOCOLS \
         -I $PROTOBUF_ROOT/include/google/protobuf \
         --js_out=import_style=commonjs,binary:$target \
-        --grpc_out=grpc_js:$target $protocols_dir/markdown.proto
+        --grpc_out=grpc_js:$target $PALM_PROTOCOLS/morus.proto
 }
 
+function generate_diesel_postgresql() {
+    echo "generate diesel schema for postgresql"
+
+    DATABASE_URL=$1 diesel print-schema \
+        -o schema_migrations \
+        casbin_rule locales settings \
+        users user_contacts user_bans user_sessions logs \
+        google_users \
+        wechat_oauth2_users wechat_mini_program_users \
+        attachments attachment_resources \
+        roles roles_users roles_constraints permissions \
+        leave_words shorter_links notifications twilio_sms_logs crawler_logs \
+        tags tag_resources \
+        categories category_resources \
+        vote_items vote_logs \
+        footprints feedbacks favorites issues comments search_histories \
+        menus \
+        >camelia/src/schema.rs
+
+    DATABASE_URL=$1 diesel print-schema -o cms_* >cms/src/schema.rs
+    DATABASE_URL=$1 diesel print-schema -o forum_* >forum/src/schema.rs
+    DATABASE_URL=$1 diesel print-schema -o daffodil_* >daffodil/src/schema.rs
+
+}
 # -----------------------------------------------------------------------------
 
 generate_musa
 generate_morus
 
-generate_go lilac casbin
-generate_go lilac tink
-generate_go lilac minio
+# generate_go lilac casbin
+# generate_go lilac tink
+# generate_go lilac minio
 
 # generate_lily
 # echo "generate lily requirements.txt"
 # cd $WORKSPACE/lily
 # pip freeze >requirements.txt
 
-# TODO
-# echo 'format rust code'
-# cd $WORKSPACE
-# cargo fmt
+echo 'format rust code'
+cd $WORKSPACE
+generate_diesel_postgresql "postgres://www:change-me@127.0.0.1:5432/palm"
+cargo fmt
 
 echo 'done.'
 exit 0
