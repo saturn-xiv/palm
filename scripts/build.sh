8f70ca5b213b0f391f8974e64f1db56b353fe797
Jeremy Zheng <jitang.zheng@gmail.com>
2024-03-29 00:45:46 +0800

    :hammer: add build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,6 +38,11 @@ function build_musa() {
 
 }
 
+function install_deb() {
+    apt -y install libc6-dev:$1 libudev-dev:$1 libssl-dev:$1 \
+        libpq5:$1 libpq-dev:$1 libmysqlclient-dev:$1 libsqlite3-dev:$1
+}
+
 function build_go_project() {
     cd $WORKSPACE/$1
 
@@ -48,6 +53,68 @@ function build_go_project() {
     GOOS=linux GOARCH=$2 go build -ldflags "$ldflags"
     mkdir -p $TARGET_DIR/bin/$3
     mv $1 $TARGET_DIR/bin/$3/
+}
+
+function build_cargo_x86_64() {
+    echo "build palm for x84-64"
+    cd $WORKSPACE
+
+    local CC=gcc
+    local CXX=g++
+
+    local PKG_CONFIG_ALL_STATIC=1
+
+    local target="x86_64-unknown-linux-gnu"
+
+    cargo clean
+    cargo build --release --target $target
+    cd target/$target/release/
+    mkdir -p $TARGET_DIR/bin/x86_64/
+    cp aloe fig $TARGET_DIR/bin/x86_64/
+}
+
+function build_cargo_aarch64() {
+    echo "build palm for aarch64"
+    cd $WORKSPACE
+
+    local CC=aarch64-linux-gnu-gcc
+    local CXX=aarch64-linux-gnu-g++
+    local HOST_CC=gcc
+
+    local PKG_CONFIG_ALLOW_CROSS=1
+    local PKG_CONFIG_ALL_STATIC=1
+    local PKG_CONFIG_DIR=
+    local PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig
+
+    local target="aarch64-unknown-linux-gnu"
+
+    cargo clean
+    cargo build --release --target $target
+    cd target/$target/release/
+    mkdir -p $TARGET_DIR/bin/aarch64/
+    cp aloe fig $TARGET_DIR/bin/aarch64/
+}
+
+function build_cargo_armhf() {
+    echo "build palm for armhf"
+    cd $WORKSPACE
+
+    local CC=arm-linux-gnueabihf-gcc
+    local CXX=arm-linux-gnueabihf-g++
+    local HOST_CC=gcc
+
+    local PKG_CONFIG_ALLOW_CROSS=1
+    local PKG_CONFIG_ALL_STATIC=1
+    local PKG_CONFIG_DIR=
+    local PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig
+
+    local target="armv7-unknown-linux-gnueabihf"
+
+    cargo clean
+    cargo build --release --target $target
+    cd target/$target/release/
+    mkdir -p $TARGET_DIR/bin/armhf/
+    cp aloe fig $TARGET_DIR/bin/armhf/
 }
 
 function copy_jdk() {
@@ -123,6 +190,66 @@ function copy_nodejs() {
     mv node-${node_version}-linux-arm64 arm64
 }
 
+copy_assets() {
+    echo "copy fig assets"
+    cd $WORKSPACE/fig/
+    if [ ! -d node_modules ]; then
+        npm install
+    fi
+    local -a packages=(
+        "bootstrap/dist"
+        "bulma/css"
+        "marked/marked.min.js"
+        "material-design-icons/iconfont"
+        "d3/dist"
+        "@fontsource/roboto"
+        "moment/dist"
+        "moment-timezone/builds/moment-timezone-with-data.min.js"
+        "@popperjs/core/dist"
+        "mdb-ui-kit/css"
+        "mdb-ui-kit/js"
+        "mdb-ui-kit/img"
+        "qrcodejs/qrcode.min.js"
+        "@fortawesome/fontawesome-free/js"
+        "@fortawesome/fontawesome-free/css"
+        "@fortawesome/fontawesome-free/svgs"
+        "@fortawesome/fontawesome-free/webfonts"
+        "@fortawesome/fontawesome-free/sprites"
+        "famfamfam-flags/dist"
+        "famfamfam-silk/dist"
+        "famfamfam-mini/dist"
+    )
+    for i in "${packages[@]}"; do
+        local p=node_modules/$i
+        local t=$(dirname "$TARGET_DIR/fig/$p")
+        mkdir -p $t
+        cp -a $p $t/
+    done
+
+    echo "copy palm assets"
+    cd $WORKSPACE
+    cp -a README.md LICENSE \
+        $TARGET_DIR/
+    cp -a assets db locales themes $TARGET_DIR/fig/
+
+    echo "$GIT_VERSION" >$TARGET_DIR/VERSION
+    echo "$(date -R)" >>$TARGET_DIR/VERSION
+}
+
+function build_dashboard() {
+    cd $WORKSPACE/$1/dashboard
+    if [ ! -d node_modules ]; then
+        npm install
+    fi
+    if [ -d dist ]; then
+        rm -r dist
+    fi
+    npm run build
+
+    mkdir -p $TARGET_DIR/$1
+    mv dist $TARGET_DIR/$1/dashboard
+}
+
 # -----------------------------------------------------------------------------
 
 if [ $ID != "ubuntu" ]; then
@@ -144,13 +271,24 @@ build_go_project lilac amd64 x86_64
 build_go_project lilac arm64 aarch64
 build_go_project lilac riscv64 riscv64
 
+install_deb amd64
+build_cargo_x86_64 aloe
+build_cargo_x86_64 fig
+
+install_deb arm64
+build_cargo_aarch64 aloe
+build_cargo_aarch64 fig
+
 build_musa
 copy_jdk
 
 build_morus
 copy_nodejs
 
+build_dashboard fig
+
 copy_envoy
+copy_assets
 
 cd $(dirname $TARGET_DIR)
 echo "compressing $PACKAGE_NAME..."
@@ -159,25 +297,6 @@ md5sum $PACKAGE_NAME.tar.xz >$PACKAGE_NAME.md5
 
 echo "done($GIT_VERSION)."
 exit 0
-
-# function build_dashboard() {
-#     cd $WORKSPACE/$1/dashboard
-#     if [ ! -d node_modules ]; then
-#         npm install
-#     fi
-#     if [ -d dist ]; then
-#         rm -r dist
-#     fi
-#     npm run build
-
-#     mkdir -p $TARGET_DIR/$1
-#     mv dist $TARGET_DIR/$1/dashboard
-# }
-
-# function install_deb() {
-#     apt -y install libc6-dev:$1 libudev-dev:$1 libssl-dev:$1 \
-#         libpq5:$1 libpq-dev:$1 libmysqlclient-dev:$1 libsqlite3-dev:$1
-# }
 
 # function build_cargo_musl() {
 #     echo "build $2 for $1"
@@ -188,113 +307,6 @@ exit 0
 
 #     mkdir -p $WORKSPACE/bin/$1
 #     cp target/$target/release/$2 $TARGET_DIR/bin/$1/
-# }
-
-# function build_cargo_x86_64() {
-#     echo "build palm for x84-64"
-#     cd $WORKSPACE
-
-#     local CC=gcc
-#     local CXX=g++
-
-#     local PKG_CONFIG_ALL_STATIC=1
-
-#     local target="x86_64-unknown-linux-gnu"
-
-#     cargo clean
-#     cargo build --release --target $target
-#     cd target/$target/release/
-#     mkdir -p $TARGET_DIR/bin/x86_64/
-#     cp aloe fig $TARGET_DIR/bin/x86_64/
-# }
-
-# function build_cargo_aarch64() {
-#     echo "build palm for aarch64"
-#     cd $WORKSPACE
-
-#     local CC=aarch64-linux-gnu-gcc
-#     local CXX=aarch64-linux-gnu-g++
-#     local HOST_CC=gcc
-
-#     local PKG_CONFIG_ALLOW_CROSS=1
-#     local PKG_CONFIG_ALL_STATIC=1
-#     local PKG_CONFIG_DIR=
-#     local PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig
-
-#     local target="aarch64-unknown-linux-gnu"
-
-#     cargo clean
-#     cargo build --release --target $target
-#     cd target/$target/release/
-#     mkdir -p $TARGET_DIR/bin/aarch64/
-#     cp aloe fig $TARGET_DIR/bin/aarch64/
-# }
-
-# function build_cargo_armhf() {
-#     echo "build palm for armhf"
-#     cd $WORKSPACE
-
-#     local CC=arm-linux-gnueabihf-gcc
-#     local CXX=arm-linux-gnueabihf-g++
-#     local HOST_CC=gcc
-
-#     local PKG_CONFIG_ALLOW_CROSS=1
-#     local PKG_CONFIG_ALL_STATIC=1
-#     local PKG_CONFIG_DIR=
-#     local PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig
-
-#     local target="armv7-unknown-linux-gnueabihf"
-
-#     cargo clean
-#     cargo build --release --target $target
-#     cd target/$target/release/
-#     mkdir -p $TARGET_DIR/bin/armhf/
-#     cp aloe fig $TARGET_DIR/bin/armhf/
-# }
-
-# copy_assets() {
-#     echo "copy fig assets"
-#     cd $WORKSPACE/fig/
-#     if [ ! -d node_modules ]; then
-#         npm install
-#     fi
-#     local -a packages=(
-#         "bootstrap/dist"
-#         "bulma/css"
-#         "marked/marked.min.js"
-#         "material-design-icons/iconfont"
-#         "d3/dist"
-#         "@fontsource/roboto"
-#         "moment/dist"
-#         "moment-timezone/builds/moment-timezone-with-data.min.js"
-#         "@popperjs/core/dist"
-#         "mdb-ui-kit/css"
-#         "mdb-ui-kit/js"
-#         "mdb-ui-kit/img"
-#         "qrcodejs/qrcode.min.js"
-#         "@fortawesome/fontawesome-free/js"
-#         "@fortawesome/fontawesome-free/css"
-#         "@fortawesome/fontawesome-free/svgs"
-#         "@fortawesome/fontawesome-free/webfonts"
-#         "@fortawesome/fontawesome-free/sprites"
-#         "famfamfam-flags/dist"
-#         "famfamfam-silk/dist"
-#         "famfamfam-mini/dist"
-#     )
-#     for i in "${packages[@]}"; do
-#         local p=node_modules/$i
-#         local t=$(dirname "$TARGET_DIR/fig/$p")
-#         mkdir -p $t
-#         cp -a $p $t/
-#     done
-
-#     echo "copy palm assets"
-#     cd $WORKSPACE
-#     cp -a README.md LICENSE \
-#         $TARGET_DIR/
-
-#     echo "$GIT_VERSION" >$TARGET_DIR/VERSION
-#     echo "$(date -R)" >>$TARGET_DIR/VERSION
 # }
 
 # function build_lily() {
@@ -402,7 +414,6 @@ exit 0
 # build_dashboard fig
 # build_dashboard aloe
 
-# install_deb amd64
 # build_cargo_x86_64
 
 # install_deb arm64
