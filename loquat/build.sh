653ec1af41995bc33f565afbe8b8f4dbdd4cc941
Jeremy Zheng <jitang.zheng@gmail.com>
2024-04-11 06:43:31 +0000

    :construction: add new loquat services

--- loquat/build.sh
+++ loquat/build.sh
@@ -4,13 +4,17 @@ set -e
 
 . /etc/os-release
 
-if [ $ID == "ubuntu" && $VERSION_CODENAME == "jammy"]; then
+if [ $ID == "ubuntu" ] && [ $VERSION_CODENAME == "jammy" ]; then
     apt update
-    apt install -y build-essential cmake g++-12 golang libunwind-dev libboost-all-dev
+    apt install -y git build-essential g++-12 golang \
+        python3 python3-pip \
+        libssl-dev libunwind-dev libevent-dev libboost-all-dev
 else
-    echo "Unsupported system: $ID"
+    echo "Unsupported system: $ID/$VERSION_CODENAME"
     exit 1
 fi
+
+pip3 install cmake
 
 export SOURCE_ROOT=$PWD
 export BUILD_ROOT=$PWD/build/Release
@@ -18,8 +22,10 @@ export BUILD_ROOT=$PWD/build/Release
 mkdir -p $BUILD_ROOT
 
 CC=gcc-12 CXX=g++-12 cmake -DCMAKE_BUILD_TYPE=Release \
-    -DABSL_PROPAGATE_CXX_STD=ON -DTINK_USE_SYSTEM_OPENSSL=OFF \
-    -DBUILD_COMPILER=OFF -DWITH_OPENSSL=OFF -DBUILD_JAVA=OFF -DBUILD_JAVASCRIPT=OFF -DBUILD_NODEJS=OFF -DBUILD_PYTHON=OFF \
+    -DABSL_PROPAGATE_CXX_STD=ON -DTINK_USE_SYSTEM_OPENSSL=ON -DTINK_BUILD_TESTS=OFF \
+    -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_COMPILER=OFF -DWITH_OPENSSL=OFF -DBUILD_JAVA=OFF -DBUILD_JAVASCRIPT=OFF -DBUILD_NODEJS=OFF -DBUILD_PYTHON=OFF \
+    -DCASBIN_BUILD_TESTS=OFF -DCASBIN_BUILD_PYTHON_BINDINGS=OFF \
+    -DLEVELDB_BUILD_TESTS=OFF -DLEVELDB_BUILD_BENCHMARKS=OFF \
     -B $BUILD_ROOT -S $SOURCE_ROOT
 
 make -C $BUILD_ROOT loquat
--- palm/protocols/loquat.thrift
+++ palm/protocols/loquat.thrift
@@ -1,21 +1,48 @@
 namespace cpp loquat.v1
 namespace java com.github.saturn_xiv.palm.plugins.loquat.v1
 
-service Jwt {
-    string sign(1:string auth, 2:string subject, 3:string audience, 4:i64 ttl);
-    string verify(1:string auth, 2:string token, 3:string audience);
-}
-
-service Hmac {
-    binary sign(1:string auth, 2:binary plain);
-    void verify(1:string auth, 2:binary code, 3:binary plain);
-}
-
-service Aes {
-    binary encrypt(1:string auth, 2:binary plain);
-    binary decrypt(1:string auth, 2:binary code);
-}
-
 service Health {
-  void check(1:string auth);
+  void check();
+}
+
+struct Resource {
+    1:string type,
+    2:optional i32 id,
+}
+struct Permission {
+    1:string operation,
+    2:Resource resource,
+}
+
+service Auth {
+    void sign_up(1:string user, 2:string password);
+    void sign_in(1:string user, 2:string password, 3:i64 ttl);
+    void change_password(1:string user, 2:string current_password, 3:string new_password);
+    void reset_password(1:string token, 2:string user, 3:string password);
+    
+    bool has(1:string token, 2:string role);
+    bool can(1:string token, 2:string operation, 3:string resource);
+    set<string> roles(1:string token);
+    set<Permission> permissions(1:string token);
+}
+
+service Policy {
+    void add_roles_for_user(1:string token, 2:string user, 3:set<string> roles);
+    set<string> get_roles_for_user(1:string token, 2:string user);
+    set<string> get_users_for_role(1:string token, 2:string role);    
+    void delete_roles_for_user(1:string token, 2:string user, 3:set<string> roles);
+    void delete_users_for_role(1:string token, 2:string role, 3:set<string> users);
+    void delete_role(1:string token, 2:string role);
+    void delete_user(1:string token, 2:string user);
+    void add_permissions_for_user(1:string token, 2:string user, 3:set<Permission> permissions);
+    void add_permissions_for_role(1:string token, 2:string role, 3:set<Permission> permissions);
+    void delete_permissions_for_user(1:string token, 2:string user, 3:set<Permission> permissions);
+    void delete_permissions_for_role(1:string token, 2:string role, 3:set<Permission> permissions);
+    set<Permission> get_permissions_for_user(1:string token, 2:string user);
+    set<Permission> get_permissions_for_role(1:string token, 2:string role);
+}
+
+service Token{
+    string sign(1:string token, 2:string issuer, 3:string subject, 4:string audience, 5:i64 ttl);
+    string verify(1:string token, 2:string issuer, 3:string audience);
 }
