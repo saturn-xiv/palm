c5c6176aafd1f5677a302342ee144983078cb196
Jeremy Zheng <jitang.zheng@gmail.com>
2024-04-12 03:46:53 +0000

    :wrench: add centos-7 support

--- scripts/go.sh
+++ scripts/go.sh
@@ -8,10 +8,11 @@ function build() {
     cd $WORKSPACE/$1
 
     local pkg="github.com/saturn-xiv/palm/$1/cmd"
-    local ldflags="-extldflags=-static -s -w -X '$pkg.repo_url=$(git remote get-url origin)' -X '$pkg.author_name=$(git config --get user.name)' -X '$pkg.author_email=$(git config --get user.email)' -X '$pkg.build_time=$(date -u)' -X '$pkg.git_version=$(git describe --tags --always --dirty --first-parent)'"
+    # ldflags="-extldflags=-static" -tags sqlite_omit_load_extension
+    local ldflags="-s -w -X '$pkg.repo_url=$(git remote get-url origin)' -X '$pkg.author_name=$(git config --get user.name)' -X '$pkg.author_email=$(git config --get user.email)' -X '$pkg.build_time=$(date -u)' -X '$pkg.git_version=$(git describe --tags --always --dirty --first-parent)'"
 
     echo "build $1.$2"
-    GOOS=linux GOARCH=$2 go build -ldflags "$ldflags" -tags sqlite_omit_load_extension -o $WORKSPACE/$1/tmp/$1.$2
+    GOOS=linux GOARCH=$2 go build -ldflags "$ldflags" -o $WORKSPACE/$1/tmp/$1.$2
 }
 
 if [ "$#" -ne 1 ]; then
@@ -23,12 +24,23 @@ fi
 
 if ! [ -x "$(command -v go)" ]; then
     if [ $ID == "ubuntu" ]; then
-        if [ ! -f /etc/apt/sources.list.d/longsleep-ubuntu-golang-backports-$VERSION_CODENAME.list ]; then
-            apt update
-            apt install -y software-properties-common
-            add-apt-repository -y ppa:longsleep/golang-backports
+        # if [ ! -f /etc/apt/sources.list.d/longsleep-ubuntu-golang-backports-$VERSION_CODENAME.list ]; then
+        #     apt update
+        #     apt install -y software-properties-common
+        #     add-apt-repository -y ppa:longsleep/golang-backports
+        # fi
+        # golang-go
+        apt update
+        apt install -y wget git
+    elif [ $ID == "centos" ]; then
+        if [ ! -f /etc/yum.repos.d/ius.repo ]; then
+            yum install -y \
+                https://repo.ius.io/ius-release-el7.rpm \
+                https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
         fi
-        apt install -y git golang-go
+        yum update -y
+        yum install -y wget git236
+
     elif [ $ID == "arch" ]; then
         sudo pacman -S go
     else
@@ -36,6 +48,22 @@ if ! [ -x "$(command -v go)" ]; then
         exit 1
     fi
 fi
+
+export GO_VERSION="1.22.2"
+if [ ! -d /opt/go ]; then
+    wget -P /tmp/ https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+    tar -C /opt -xf /tmp/go${GO_VERSION}.linux-amd64.tar.gz
+fi
+export PATH=/opt/go/bin:$PATH
+# if [ $ID == "ubuntu" ]; then
+#     apt update
+#     apt install -y wget git
+# elif [ $ID == "centos" ]; then
+
+# else
+#     echo "Unsupported system: $ID"
+#     exit 1
+# fi
 
 declare -a targets=(
     "amd64"
