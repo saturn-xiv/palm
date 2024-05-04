202ec4ede31bed98a6d89c43b2167b3ae6160afb
Jeremy Zheng <jitang.zheng@gmail.com>
2024-05-04 19:01:15 +0530

    :bug: use node lts

--- morus/README.md
+++ morus/README.md
@@ -2,6 +2,7 @@
 
 ```bash
 $ podman run --rm -it --events-backend=file --network host -v $(dirname $PWD):/workspace:z ubuntu:latest
+> cd /workspace/morus
 > ./build.sh
 > node dist/morus.xxx.bundle.js config.json
 ```
--- morus/build.sh
+++ morus/build.sh
@@ -3,7 +3,19 @@
 set -e
 
 apt update
-apt install -y nodejs npm
+# DEBIAN_FRONTEND=noninteractive apt install -y nodejs npm \
+#     pkg-config build-essential libpixman-1-dev libcairo2-dev librust-pangocairo-dev
+
+export NODE_VERSION="20.12.2"
+
+apt install -y wget
+if [ ! -d /opt/nodejs ]; then
+    wget -q -P $HOME/ https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
+    tar xf $HOME/node-v${NODE_VERSION}-linux-x64.tar.xz -C /opt
+fi
+
+export NODE_HOME=/opt/node-v${NODE_VERSION}-linux-x64
+export PATH=$NODE_HOME/bin:$PATH
 
 if [ ! -d node_modules ]; then
     npm install
