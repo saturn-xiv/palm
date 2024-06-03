ef29df015b4d2c1072a16b4298213d9abe68eecd
Jeremy Zheng <jitang.zheng@gmail.com>
2024-06-03 10:29:13 +0530

    :tada: create loquat project

---
+++ loquat/README.md
@@ -0,0 +1,17 @@
+# USAGE
+
+- For x86_64
+
+  ```bash
+  xmake f -p linux -a x86_64 -m release
+  ```
+
+- For aarch64
+
+  ```bash
+  xmake f -p cross -m release --sdk=$HOME/local/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu
+  ```
+
+## Documents
+
+- [Arm GNU Toolchain Downloads](https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads)
---
+++ loquat/xmake.lua
@@ -0,0 +1,24 @@
+add_rules("mode.debug", "mode.release")
+
+add_requires(
+    "argparse", "spdlog",
+    "boost", "libsodium", "libevent", "mailio",
+    "amqp-cpp", "pahomqttcpp", "thrift", "grpc",
+    "toml++", "nlohmann_json", "cppcodec",
+    "inja",
+    "soci", "libpqxx", "mysqlpp", "sqlitecpp"
+)
+
+target("loquat")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    set_languages("c11", "c++17")
+    add_packages(
+        "argparse", "spdlog",
+        "boost", "libsodium", "libevent", "mailio",
+        "amqp-cpp", "pahomqttcpp", "thrift", "grpc",
+        "toml++", "nlohmann_json", "cppcodec",
+        "inja",
+        "soci", "libpqxx","mysqlpp", "sqlitecpp"  
+    )
+
