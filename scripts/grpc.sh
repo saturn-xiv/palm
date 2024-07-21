56043db2bbaf4298b492b8d4419ebf9b29f7d751
Jeremy Zheng <jitang.zheng@gmail.com>
2024-07-21 17:47:49 +0530

    :hammer: merge tuberose build tools

--- scripts/grpc.sh
+++ scripts/grpc.sh
@@ -10,16 +10,14 @@ export INSTALL_ROOT=$HOME/.local
 
 function build_grpc() {
     # https://grpc.io/docs/languages/cpp/quickstart/
-    if [ -L $HOME/.local/bin/protoc ]
-    then
+    if [ -L $HOME/.local/bin/protoc ]; then
         echo 'already exists!'
         exit 0
     fi
-    if [ -d $SOURCE_ROOT ]
-    then
+    if [ -d $SOURCE_ROOT ]; then
         cd $SOURCE_ROOT
         git checkout master
-        git pull        
+        git pull
         git checkout $1
         # fix unable to find current revision in submodule path
         # git pull --recurse-submodules
@@ -34,9 +32,7 @@ function build_grpc() {
     git checkout $2
     git submodule update --init --recursive
 
-   
-    if [ -d $BUILD_ROOT ]
-    then
+    if [ -d $BUILD_ROOT ]; then
         rm -r $BUILD_ROOT
     fi
     mkdir -p $BUILD_ROOT
@@ -51,19 +47,6 @@ function build_grpc() {
     echo "done($1, $2)."
 }
 
-# if [ "$#" -ne 2 ]
-# then
-#     echo "USAGE: $0 GRPC_VERSION PROTOBUF_VERSION"
-#     exit 1
-# fi
-
-# vcpkg: 2023.04.15
-build_grpc "v1.51.1" "v3.21.12"
-
-
-if [[ $UBUNTU_CODENAME == "bionic" ]]
-then
-    cp $SOURCE_ROOT/third_party/re2/re2.pc $HOME/.local/lib/pkgconfig/
-fi
+build_grpc "v1.60.0" "v4.25.1"
 
 exit 0
---
+++ scripts/tuberose.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+. /etc/os-release
+function build_tuberose() {}
+
+if [[ $ID == "ubuntu" ]]; then
+    apt update
+    apt -y upgrade
+    DEBIAN_FRONTEND=noninteractive apt install -y git cmake \
+        clang mold \
+        build-essential crossbuild-essential-arm64 crossbuild-essential-riscv64
+elif [[ $ID == "arch" ]]; then
+    echo "build on archlinux"
+else
+    echo "unsupported os($ID)"
+    exit 1
+fi
+
+function build_tuberose() {
+    local thrift_args="-DBUILD_TESTING=OFF -DBUILD_COMPILER=OFF -DWITH_OPENSSL=OFF -DBUILD_JAVA=OFF -DBUILD_JAVASCRIPT=OFF -DBUILD_NODEJS=OFF -DBUILD_PYTHON=OFF"
+    local mailio_args="-DMAILIO_BUILD_DOCUMENTATION=OFF -DMAILIO_BUILD_EXAMPLES=OFF -DMAILIO_BUILD_SHARED_LIBRARY=OFF -DMAILIO_BUILD_TESTS=OFF"
+    local casbin_args="-DCASBIN_BUILD_TEST=OFF -DCASBIN_BUILD_BENCHMARK=OFF -DCASBIN_BUILD_PYTHON_BINDINGS=OFF -DCASBIN_INSTALL=OFF"
+    local build_dir=$WORKSPACE/build/tuberose-$1
+    mkdir -p $build_dir
+
+    cmake -S $WORKSPACE/tuberose -B $build_dir -DCMAKE_MAKE_PROGRAM=make -DCMAKE_BUILD_TYPE=Release \
+        -DVCPKG_HOST_TRIPLET=x64-linux -DVCPKG_TARGET_TRIPLET=$2-linux \
+        -DCMAKE_TOOLCHAIN_FILE=$WORKSPACE/tuberose/vcpkg/scripts/buildsystems/vcpkg.cmake \
+        -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORKSPACE/tuberose/toolchains/$2.cmake \
+        $mailio_args $casbin_args $thrift_args -DBoost_NO_WARN_NEW_VERSIONS=1
+
+    make -j -C $build_dir
+
+}
+
+declare -a triples=(
+    "x86_64"
+    "aarch64"
+    # "riscv64"
+)
+for t in "${triples[@]}"; do
+    build_tuberose $t
+done
+
+echo 'done.'
+exit 0
