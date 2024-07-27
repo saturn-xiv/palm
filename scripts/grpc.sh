f91c10c63306b226b553ae2ce510270b9a25ab38
Jeremy Zheng <jitang.zheng@gmail.com>
2024-07-27 12:45:00 +0530

    :arrow_up: upgrade grpc-go

--- scripts/grpc.sh
+++ scripts/grpc.sh
@@ -26,23 +26,31 @@ function build_grpc() {
         git clone --recurse-submodules -b $1 https://github.com/grpc/grpc.git $SOURCE_ROOT
     fi
 
+    cd $HOME/downloads/grpc/third_party/protobuf
+    git checkout main
+    git pull
+    git checkout $2
+    git submodule update --init --recursive
+
     if [ -d $BUILD_ROOT ]; then
         rm -r $BUILD_ROOT
     fi
+
     CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release \
         -DABSL_PROPAGATE_CXX_STD=ON \
         -DgRPC_INSTALL=ON -DgRPC_SSL_PROVIDER=package -DgRPC_BUILD_TESTS=OFF \
         -DCMAKE_INSTALL_PREFIX=$INSTALL_ROOT -B $BUILD_ROOT -S $SOURCE_ROOT
+
     make -j $(nproc --ignore=2) -C $BUILD_ROOT
     make install -C $BUILD_ROOT
 }
 
-if [ "$#" -ne 1 ]; then
-    echo "USAGE: $0 GRPC_VERSION"
-    exit 1
-fi
+# if [ "$#" -ne 2 ]; then
+#     echo "USAGE: $0 GRPC_VERSION PROTOC_VERSION"
+#     exit 1
+# fi
 
-build_grpc $1
+build_grpc v1.51.1 v3.21.12
 
 if [[ $UBUNTU_CODENAME == "bionic" ]]; then
     cp $SOURCE_ROOT/third_party/re2/re2.pc $HOME/.local/lib/pkgconfig/
