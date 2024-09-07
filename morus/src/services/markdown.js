62846c92dad342c386e74ef576979c2d39d1c15c
Jeremy Zheng <jitang.zheng@gmail.com>
2024-09-07 19:44:27 +0530

    :bug: fix php tls client

--- morus/src/services/markdown.js
+++ morus/src/services/markdown.js
@@ -5,12 +5,15 @@ import { JSDOM } from "jsdom";
 import { parse as parse_markdown } from "marked";
 
 import { MarkdownToHtmlResponse } from "../protocols/morus_pb";
+import logger from "../logger";
 
 export const to_html = (call, callback) => {
-  const request = call.request.getPayload();
-  const html = parse_markdown(request);
+  logger.debug(
+    `markdown to html(${call.request.getSanitize()}): ${call.request.getPayload()}`
+  );
+  const html = parse_markdown(call.request.getPayload());
   var reply = new MarkdownToHtmlResponse();
-  if (call.request.sanitize) {
+  if (call.request.getSanitize()) {
     const window = new JSDOM("").window;
     const purify = createDOMPurify(window);
     const clean = purify.sanitize(html);
--- tutorials/php/morus-client.php
+++ tutorials/php/morus-client.php
@@ -2,13 +2,11 @@
 
 require dirname(__FILE__) . '/vendor/autoload.php';
 
-function markdown2html($server, $payload, $sanitize)
+function markdown2html($server_uri, $options, $payload, $sanitize)
 {
     global $logger;
 
-    $client = new Palm\Morus\V1\MarkdownClient($server, [
-        'credentials' => Grpc\ChannelCredentials::createInsecure(),
-    ]);
+    $client = new Palm\Morus\V1\MarkdownClient($server_uri, $options);
     $request = new Palm\Morus\V1\MarkdownToHtmlRequest();
     $request->setPayload($payload);
     $request->setSanitize($sanitize);
@@ -32,7 +30,19 @@ if (empty($argv[1])) {
 $logger->debug("load config from", ['file' => $argv[1]]);
 $config = json_decode(file_get_contents($argv[1]));
 
-$server = $config->{'server'}->{'host'} . ":" . $config->{'server'}->{'port'};
-$logger->info("connect to", ['server' => $server]);
+$server_uri = $config->{'server'}->{'host'} . ":" . $config->{'server'}->{'port'};
+// $server_options = [
+//     'credentials' => Grpc\ChannelCredentials::createInsecure(),
+// ];
+$server_options = [
+    'credentials' => \Grpc\ChannelCredentials::createSsl(
+        file_get_contents($config->{'ssl'}->{'ca-file'}),
+        file_get_contents($config->{'ssl'}->{'key-file'}),
+        file_get_contents($config->{'ssl'}->{'cert-file'})
+    ),
+    // 'grpc.ssl_target_name_override' => $config->{'server'}->{'host'},
+    'grpc.default_authority' => $config->{'ssl'}->{'authority'}
+];
 
-markdown2html($server, "# Hi, Palm!", true);
+$logger->info("connect to", ['server' => $server]);
+markdown2html($server_uri, $server_options, "# Hi, Palm!", true);
