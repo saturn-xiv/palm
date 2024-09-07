417496d2e9b72c63c4b1dfc4be5b686c7766e8a3
Jeremy Zheng <jitang.zheng@gmail.com>
2024-09-07 14:53:34 +0530

    :bug: fix proxy env

--- docker/proxy/proxy.sh
+++ docker/proxy/proxy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -e
+
 source $HOME/local/python3/bin/activate
 source /workspace/.env
 
-pproxy --reuse -l "http://:$LOCAL_PORT" -r "ssh://$SSH_HOST:$SSH_PORT#$SSH_USER::/workspace/$SSH_KEYFILE"
+pproxy --reuse -l "$PROXY_URI" -r "ssh://$SSH_HOST:$SSH_PORT#$SSH_USER::/workspace/$SSH_KEYFILE"
