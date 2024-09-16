8c119859cc15ba7cb4affad9846fac957cbb0d33
Jeremy Zheng <jitang.zheng@gmail.com>
2024-09-16 14:04:01 +0530

    :tada: create fig project

---
+++ petunia/src/lib.rs
@@ -0,0 +1,43 @@
+pub const BANNER: &str = include_str!("banner.txt");
+
+include!(concat!(env!("OUT_DIR"), "/env.rs"));
+
+pub mod client;
+
+pub mod balsam {
+    pub mod v1 {
+        tonic::include_proto!("palm.balsam.v1");
+    }
+}
+pub mod daisy {
+    pub mod v1 {
+        tonic::include_proto!("palm.daisy.v1");
+    }
+}
+pub mod lily {
+    pub mod v1 {
+        tonic::include_proto!("palm.lily.v1");
+    }
+}
+pub mod morus {
+    pub mod v1 {
+        tonic::include_proto!("palm.morus.v1");
+    }
+}
+pub mod rbac {
+    pub mod v1 {
+        tonic::include_proto!("palm.rbac.v1");
+    }
+}
+pub mod s3 {
+    pub mod v1 {
+        tonic::include_proto!("palm.s3.v1");
+    }
+}
+pub mod wechat {
+    pub mod pay {
+        pub mod v1 {
+            tonic::include_proto!("palm.wechat.pay.v1");
+        }
+    }
+}
