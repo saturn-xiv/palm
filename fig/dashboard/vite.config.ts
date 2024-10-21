4a9051a0f3c63be3e125c9c58032f2e4487a4a2e
Jeremy Zheng <jitang.zheng@gmail.com>
2024-10-21 12:04:25 +0800

    :bento: merge pictures

--- fig/dashboard/vite.config.ts
+++ fig/dashboard/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my/",
   server: {
     port: 3000,
     host: "127.0.0.1",
