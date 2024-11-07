7626a2b8487f95bd699aa2053625f93b9808b56d
Jeremy Zheng <jitang.zheng@gmail.com>
2024-11-07 15:51:53 +0000

    :tada: prepare to use antd pro

--- fig/dashboard/src/App.tsx
+++ fig/dashboard/src/App.tsx
@@ -1,30 +1,5 @@
-import { Suspense } from "react";
-import { Provider } from "react-redux";
-import { IntlProvider } from "react-intl";
-import RefreshRoundedIcon from "@mui/icons-material/RefreshRounded";
-
-import Router from "./Router";
-import store from "./store";
-import { DEFAULT_LANGUAGE } from "./i18n";
-
-interface IProps {
-  locale: string;
-  messages: Record<string, string>;
-}
-const Widget = ({ locale, messages }: IProps) => {
-  return (
-    <IntlProvider
-      messages={messages}
-      locale={locale}
-      defaultLocale={DEFAULT_LANGUAGE}
-    >
-      <Provider store={store}>
-        <Suspense fallback={<RefreshRoundedIcon />}>
-          <Router />
-        </Suspense>
-      </Provider>
-    </IntlProvider>
-  );
+const Widget = () => {
+  return <>aaa</>;
 };
 
 export default Widget;
--- fig/dashboard/src/main.tsx
+++ fig/dashboard/src/main.tsx
@@ -1,23 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
-import { index_locale_by_lang } from "./api/daffodil";
-import { get as detect_locale } from "./i18n";
+import "./main.css";
 
-import "./index.css";
+import App from "./App.tsx";
 
-const locale = detect_locale();
-
-index_locale_by_lang(locale).then((res) => {
-  const messages = res.reduce((obj, it) => {
-    obj[it.code] = it.message;
-    return obj;
-  }, {} as Record<string, string>);
-
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App messages={messages} locale={locale} />
-    </StrictMode>
-  );
-});
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
