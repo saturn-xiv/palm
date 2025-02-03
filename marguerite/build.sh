6e2e6b5c635c033c62defaa74eefd984972dfc79
Jeremy Zheng <jitang.zheng@gmail.com>
2025-02-03 16:08:21 +0000

    :wrench: use clang-18

--- marguerite/build.sh
+++ marguerite/build.sh
@@ -21,11 +21,8 @@ if [ ! -d $VCPKG_ROOT ]; then
     $VCPKG_ROOT/bootstrap-vcpkg.sh
 fi
 
-#
-# -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORKSPACE/toolchains/$ID/$1.cmake \
-# -DVCPKG_HOST_TRIPLET=x64-linux-release -DVCPKG_TARGET_TRIPLET=$1-linux-release \
 mkdir -p $BUILD_ROOT
-cmake -DCMAKE_C_COMPILER=gcc-12 -DCMAKE_CXX_COMPILER=g++-12 -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++-18 -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
     -DABSL_PROPAGATE_CXX_STD=ON \
     -DTINK_USE_SYSTEM_OPENSSL=ON -DTINK_BUILD_TESTS=OFF \
