495897bfbd18fd37850392b3d34fbb54fda416ae
Jeremy Zheng <jitang.zheng@gmail.com>
2025-02-16 12:21:08 +0530

    :hammer: merge frontend scripts

---
+++ dahlia/scripts/assets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# https://fontsource.org/docs/getting-started/install
+# https://fontsource.org/docs/getting-started/variable
+
+npm install marked moment moment-timezone d3 mermaid qrcodejs epubjs \
+    bootstrap @popperjs/core \
+    mdb-ui-kit@latest \
+    universalviewer \
+    swagger-ui-dist \
+    bulma \
+    tailwindcss@latest \
+    @tabler/core \
+    material-components-web material-design-icons \
+    @fontsource/roboto @fontsource-variable/montserrat \
+    @fortawesome/fontawesome-free \
+    famfamfam-flags famfamfam-silk famfamfam-mini \
+    --save
+
+echo 'done.'
+exit 0
---
+++ dahlia/scripts/dashboard.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# https://vitejs.dev/guide/#scaffolding-your-first-vite-project
+# npm create vite@latest fig -- --template react-ts
+
+# slate slate-react slate-history
+npm install --save @fortawesome/fontawesome-free \
+    famfamfam-flags famfamfam-silk famfamfam-mini \
+    js-cookie @types/js-cookie \
+    filesize \
+    html-to-text @types/html-to-text \
+    mermaid \
+    jwt-decode \
+    react-currency-input-field \
+    video.js @types/video.js \
+    react-copy-to-clipboard @types/react-copy-to-clipboard \
+    @wangeditor/editor @wangeditor/editor-for-react \
+    google-map-react qrcode.react \
+    @uiw/react-md-editor \
+    react-color @types/react-color \
+    react-pdf \
+    react-syntax-highlighter \
+    emoji-mart react-sparklines react-highlight-words \
+    react-player \
+    react-draggable \
+    react-big-calendar @types/react-big-calendar \
+    react-intl \
+    react-router-dom@latest \
+    @reduxjs/toolkit react-redux
+
+# https://procomponents.ant.design/en-US/docs
+# https://ant-design-charts.antgroup.com/en/manual/getting-started
+npm install --save antd @ant-design/pro-components @ant-design/charts
+
+echo 'done.'
+exit 0
