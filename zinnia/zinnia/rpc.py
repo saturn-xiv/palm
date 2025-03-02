8ec9e6df8b5bccc23b3a3e40137c34069978ab55
Jeremy Zheng <jitang.zheng@gmail.com>
2025-03-02 14:34:00 +0000

    :construction: add grpc reflection support

--- zinnia/zinnia/rpc.py
+++ zinnia/zinnia/rpc.py
@@ -2,8 +2,9 @@ from concurrent import futures
 import logging
 
 import grpc
+from grpc_reflection.v1alpha import reflection
 
-from .protocols import zinnia_pb2_grpc
+from .protocols import zinnia_pb2_grpc, zinnia_pb2
 from .cms.rpc import CmsServer
 
 logger = logging.getLogger(__name__)
@@ -13,7 +14,13 @@ def launch(host, port, max_workers):
     addr = '%s:%s' % (host, port)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers))
     zinnia_pb2_grpc.add_CmsServicer_to_server(CmsServer(), server)
+    SERVICE_NAMES = (
+        zinnia_pb2.DESCRIPTOR.services_by_name["Cms"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
     server.add_insecure_port(addr)
     server.start()
-    logger.info("rpc server started, listening on tcp://%s", addr)
+    logger.info(
+        "rpc server started, listening on tcp://%s with %d workers", addr, max_workers)
     server.wait_for_termination()
