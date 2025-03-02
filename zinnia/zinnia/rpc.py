3f7ebd3965dd75ffa7508100858025169f424987
Jeremy Zheng <jitang.zheng@gmail.com>
2025-03-02 14:42:07 +0000

    :construction: add rpc healthcheck support

--- zinnia/zinnia/rpc.py
+++ zinnia/zinnia/rpc.py
@@ -1,8 +1,12 @@
-from concurrent import futures
 import logging
+import threading
+from time import sleep
+from concurrent import futures
 
 import grpc
 from grpc_reflection.v1alpha import reflection
+from grpc_health.v1 import health, health_pb2, health_pb2_grpc
+
 
 from .protocols import zinnia_pb2_grpc, zinnia_pb2
 from .cms.rpc import CmsServer
@@ -20,7 +24,29 @@ def launch(host, port, max_workers):
     )
     reflection.enable_server_reflection(SERVICE_NAMES, server)
     server.add_insecure_port(addr)
+    _configure_health_server(server, max_workers)
     server.start()
     logger.info(
         "rpc server started, listening on tcp://%s with %d workers", addr, max_workers)
     server.wait_for_termination()
+
+
+def _health_server(health_servicer: health.HealthServicer, service: str):
+    while True:
+        health_servicer.set(service, health_pb2.HealthCheckResponse.SERVING)
+        sleep(5)
+
+
+def _configure_health_server(server: grpc.Server, max_workers: int):
+    health_servicer = health.HealthServicer(
+        experimental_non_blocking=True,
+        experimental_thread_pool=futures.ThreadPoolExecutor(max_workers),
+    )
+    health_pb2_grpc.add_HealthServicer_to_server(health_servicer, server)
+
+    toggle_health_status_thread = threading.Thread(
+        target=_health_server,
+        args=(health_servicer, "palm.zinnia.v1.Cms"),
+        daemon=True,
+    )
+    toggle_health_status_thread.start()
