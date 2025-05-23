37a3d40d61b2b959384380470ea78ce3fd3874ea
Jeremy Zheng <jitang.zheng@gmail.com>
2025-05-23 09:20:49 +0000

    :card_file_box: create pg tables

---
+++ phlox/db/migrations/20250523090201_create_currencies.sql
@@ -0,0 +1,18 @@
+-- migrate:up
+CREATE TABLE currencies(
+    id SERIAL PRIMARY KEY,
+    "code" CHAR(3) NOT NULL,
+    "number" CHAR(3) NOT NULL,
+    "name" VARCHAR(127) NOT NULL,
+    country VARCHAR(127) NOT NULL,
+    units INTEGER NOT NULL,
+    is_fund BOOLEAN,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_currencies_code ON currencies("code");
+CREATE INDEX idx_currencies_name ON currencies("name");
+CREATE INDEX idx_currencies_number ON currencies("number");
+CREATE INDEX idx_currencies_country ON currencies(country);
+
+-- migrate:down
+DROP TABLE currencies;
