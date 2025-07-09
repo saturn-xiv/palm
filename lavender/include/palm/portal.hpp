47cbf8d33dfe941f56f987075f5e3d612f250cb5
Jeremy Zheng <jitang.zheng@gmail.com>
2025-07-09 11:41:07 +0000

    :construction: add user daos

--- lavender/include/palm/portal.hpp
+++ lavender/include/palm/portal.hpp
@@ -37,6 +37,114 @@ namespace portal {
 
 namespace dao {
 
+namespace users {
+struct Item {
+  int id;
+  std::string uid;
+  std::string lang;
+  std::string timezone;
+  int sign_in_count;
+  boost::optional<std::tm> current_sign_in_at;
+  boost::optional<std::string> current_sign_in_ip;
+  boost::optional<std::tm> last_sign_in_at;
+  boost::optional<std::string> last_sign_in_ip;
+  boost::optional<std::tm> locked_at;
+  boost::optional<std::tm> deleted_at;
+  int version;
+  std::tm updated_at;
+};
+boost::optional<Item> get(soci::session& db, int id);
+boost::optional<Item> get(soci::session& db, const std::string& uid);
+void enable(soci::session& db, int id);
+void disable(soci::session& db, int id);
+namespace email {
+struct Item {
+  int id;
+  int user_id;
+  std::string real_name;
+  std::string email;
+  std::string password;
+  std::string avatar;
+  boost::optional<std::tm> confirmed_at;
+  boost::optional<std::tm> deleted_at;
+  int version;
+  std::tm updated_at;
+};
+boost::optional<Item> get(soci::session& db, int id);
+boost::optional<Item> get(soci::session& db, const std::string& email);
+void enable(soci::session& db, int id);
+void disable(soci::session& db, int id);
+}  // namespace email
+namespace wechat {
+namespace mini_program {
+struct Item {
+  int id;
+  int user_id;
+  std::string union_id;
+  std::string app_id;
+  std::string open_id;
+  boost::optional<std::string> nickname;
+  boost::optional<std::string> avatar_url;
+  boost::optional<std::tm> deleted_at;
+  int version;
+  std::tm updated_at;
+};
+boost::optional<Item> get(soci::session& db, int id);
+void enable(soci::session& db, int id);
+void disable(soci::session& db, int id);
+boost::optional<Item> get(soci::session& db, const std::string& union_id);
+boost::optional<Item> get(soci::session& db, const std::string& app_id,
+                          const std::string& open_id);
+}  // namespace mini_program
+namespace oauth2 {
+struct Item {
+  int id;
+  int user_id;
+  std::string union_id;
+  std::string app_id;
+  std::string open_id;
+  std::string nickname;
+  int sex;
+  std::string city;
+  std::string province;
+  std::string country;
+  boost::optional<std::string> head_img_url;
+  std::string privilege;
+  std::string lang;
+  boost::optional<std::tm> deleted_at;
+  int version;
+  std::tm updated_at;
+};
+boost::optional<Item> get(soci::session& db, int id);
+boost::optional<Item> get(soci::session& db, const std::string& union_id);
+boost::optional<Item> get(soci::session& db, const std::string& app_id,
+                          const std::string& open_id);
+void enable(soci::session& db, int id);
+void disable(soci::session& db, int id);
+}  // namespace oauth2
+}  // namespace wechat
+namespace google {
+namespace oauth2 {
+struct Item {
+  int id;
+  int user_id;
+  std::string subject;
+  boost::optional<std::string> email;
+  int email_verified;
+  boost::optional<std::string> name;
+  boost::optional<std::string> picture;
+  boost::optional<std::string> locale;
+  boost::optional<std::tm> deleted_at;
+  int version;
+  std::tm updated_at;
+};
+boost::optional<Item> get(soci::session& db, int id);
+void enable(soci::session& db, int id);
+void disable(soci::session& db, int id);
+}  // namespace oauth2
+}  // namespace google
+}  // namespace users
+
 namespace locales {
 struct Item {
   int id;
@@ -49,6 +157,7 @@ std::vector<std::string> languages(soci::session& db);
 void create(soci::session& db, const std::string& lang, const std::string& code,
             const std::string& message);
 void update(soci::session& db, int id, const std::string& message);
+void destroy(soci::session& db, int id);
 int count(soci::session& db);
 boost::optional<Item> get(soci::session& db, int id);
 boost::optional<Item> get(soci::session& db, const std::string& lang,
@@ -172,6 +281,200 @@ struct type_conversion<palm::portal::dao::locales::Item> {
     v.set("lang", p.lang);
     v.set("code", p.code);
     v.set("message", p.message);
+    v.set("updated_at", p.updated_at);
+    ind = i_ok;
+  }
+};
+
+template <>
+struct type_conversion<palm::portal::dao::users::Item> {
+  typedef soci::values base_type;
+
+  static void from_base(soci::values const& v, soci::indicator /* ind */,
+                        palm::portal::dao::users::Item& p) {
+    p.id = v.get<int>("id");
+    p.uid = v.get<std::string>("uid");
+    p.lang = v.get<std::string>("lang");
+    p.timezone = v.get<std::string>("timezone");
+    p.sign_in_count = v.get<int>("sign_in_count");
+    p.current_sign_in_at =
+        v.get<boost::optional<std::tm>>("current_sign_in_at");
+    p.current_sign_in_ip =
+        v.get<boost::optional<std::string>>("current_sign_in_ip");
+    p.last_sign_in_at = v.get<boost::optional<std::tm>>("last_sign_in_at");
+    p.last_sign_in_ip = v.get<boost::optional<std::string>>("last_sign_in_ip");
+    p.locked_at = v.get<boost::optional<std::tm>>("locked_at");
+    p.deleted_at = v.get<boost::optional<std::tm>>("deleted_at");
+    p.version = v.get<int>("version");
+    p.updated_at = v.get<std::tm>("updated_at");
+  }
+
+  static void to_base(const palm::portal::dao::users::Item& p, soci::values& v,
+                      soci::indicator& ind) {
+    v.set("id", p.id);
+    v.set("uid", p.uid);
+    v.set("lang", p.lang);
+    v.set("timezone", p.timezone);
+    v.set("sign_in_count", p.sign_in_count);
+    v.set("current_sign_in_at", p.current_sign_in_at);
+    v.set("current_sign_in_ip", p.current_sign_in_ip);
+    v.set("last_sign_in_at", p.last_sign_in_at);
+    v.set("last_sign_in_ip", p.last_sign_in_ip);
+    v.set("locked_at", p.locked_at);
+    v.set("deleted_at", p.deleted_at);
+    v.set("version", p.version);
+    v.set("updated_at", p.updated_at);
+    ind = i_ok;
+  }
+};
+
+template <>
+struct type_conversion<palm::portal::dao::users::email::Item> {
+  typedef soci::values base_type;
+
+  static void from_base(soci::values const& v, soci::indicator /* ind */,
+                        palm::portal::dao::users::email::Item& p) {
+    p.id = v.get<int>("id");
+    p.user_id = v.get<int>("user_id");
+    p.real_name = v.get<std::string>("real_name");
+    p.email = v.get<std::string>("email");
+    p.password = v.get<std::string>("password");
+    p.avatar = v.get<std::string>("avatar");
+    p.confirmed_at = v.get<boost::optional<std::tm>>("confirmed_at");
+    p.deleted_at = v.get<boost::optional<std::tm>>("deleted_at");
+    p.version = v.get<int>("version");
+    p.updated_at = v.get<std::tm>("updated_at");
+  }
+
+  static void to_base(const palm::portal::dao::users::email::Item& p,
+                      soci::values& v, soci::indicator& ind) {
+    v.set("id", p.id);
+    v.set("user_id", p.user_id);
+    v.set("real_name", p.real_name);
+    v.set("email", p.email);
+    v.set("password", p.password);
+    v.set("avatar", p.avatar);
+    v.set("confirmed_at", p.confirmed_at);
+    v.set("deleted_at", p.deleted_at);
+    v.set("version", p.version);
+    v.set("updated_at", p.updated_at);
+    ind = i_ok;
+  }
+};
+
+template <>
+struct type_conversion<palm::portal::dao::users::wechat::mini_program::Item> {
+  typedef soci::values base_type;
+
+  static void from_base(
+      soci::values const& v, soci::indicator /* ind */,
+      palm::portal::dao::users::wechat::mini_program::Item& p) {
+    p.id = v.get<int>("id");
+    p.user_id = v.get<int>("user_id");
+    p.union_id = v.get<std::string>("union_id");
+    p.app_id = v.get<std::string>("app_id");
+    p.open_id = v.get<std::string>("open_id");
+    p.nickname = v.get<boost::optional<std::string>>("nickname");
+    p.avatar_url = v.get<boost::optional<std::string>>("avatar_url");
+    p.deleted_at = v.get<boost::optional<std::tm>>("deleted_at");
+    p.version = v.get<int>("version");
+    p.updated_at = v.get<std::tm>("updated_at");
+  }
+
+  static void to_base(
+      const palm::portal::dao::users::wechat::mini_program::Item& p,
+      soci::values& v, soci::indicator& ind) {
+    v.set("id", p.id);
+    v.set("user_id", p.user_id);
+    v.set("union_id", p.union_id);
+    v.set("app_id", p.app_id);
+    v.set("open_id", p.open_id);
+    v.set("nickname", p.nickname);
+    v.set("avatar_url", p.avatar_url);
+    v.set("deleted_at", p.deleted_at);
+    v.set("version", p.version);
+    v.set("updated_at", p.updated_at);
+    ind = i_ok;
+  }
+};
+
+template <>
+struct type_conversion<palm::portal::dao::users::wechat::oauth2::Item> {
+  typedef soci::values base_type;
+
+  static void from_base(soci::values const& v, soci::indicator /* ind */,
+                        palm::portal::dao::users::wechat::oauth2::Item& p) {
+    p.id = v.get<int>("id");
+    p.user_id = v.get<int>("user_id");
+    p.union_id = v.get<std::string>("union_id");
+    p.app_id = v.get<std::string>("app_id");
+    p.open_id = v.get<std::string>("open_id");
+    p.nickname = v.get<std::string>("nickname");
+    p.sex = v.get<int>("sex");
+    p.city = v.get<std::string>("city");
+    p.province = v.get<std::string>("province");
+    p.country = v.get<std::string>("country");
+    p.head_img_url = v.get<boost::optional<std::string>>("head_img_url");
+    p.lang = v.get<std::string>("lang");
+    p.privilege = v.get<std::string>("privilege");
+    p.deleted_at = v.get<boost::optional<std::tm>>("deleted_at");
+    p.version = v.get<int>("version");
+    p.updated_at = v.get<std::tm>("updated_at");
+  }
+
+  static void to_base(const palm::portal::dao::users::wechat::oauth2::Item& p,
+                      soci::values& v, soci::indicator& ind) {
+    v.set("id", p.id);
+    v.set("user_id", p.user_id);
+    v.set("union_id", p.union_id);
+    v.set("app_id", p.app_id);
+    v.set("open_id", p.open_id);
+    v.set("nickname", p.nickname);
+    v.set("sex", p.sex);
+    v.set("city", p.city);
+    v.set("province", p.province);
+    v.set("country", p.country);
+    v.set("head_img_url", p.head_img_url);
+    v.set("privilege", p.privilege);
+    v.set("lang", p.lang);
+    v.set("deleted_at", p.deleted_at);
+    v.set("version", p.version);
+    v.set("updated_at", p.updated_at);
+    ind = i_ok;
+  }
+};
+
+template <>
+struct type_conversion<palm::portal::dao::users::google::oauth2::Item> {
+  typedef soci::values base_type;
+
+  static void from_base(soci::values const& v, soci::indicator /* ind */,
+                        palm::portal::dao::users::google::oauth2::Item& p) {
+    p.id = v.get<int>("id");
+    p.user_id = v.get<int>("user_id");
+    p.subject = v.get<std::string>("subject");
+    p.email = v.get<boost::optional<std::string>>("email");
+    p.email_verified = v.get<int>("email_verified");
+    p.name = v.get<boost::optional<std::string>>("name");
+    p.picture = v.get<boost::optional<std::string>>("picture");
+    p.locale = v.get<boost::optional<std::string>>("locale");
+    p.deleted_at = v.get<boost::optional<std::tm>>("deleted_at");
+    p.version = v.get<int>("version");
+    p.updated_at = v.get<std::tm>("updated_at");
+  }
+
+  static void to_base(const palm::portal::dao::users::google::oauth2::Item& p,
+                      soci::values& v, soci::indicator& ind) {
+    v.set("id", p.id);
+    v.set("user_id", p.user_id);
+    v.set("subject", p.subject);
+    v.set("email", p.email);
+    v.set("email_verified", p.email_verified);
+    v.set("name", p.name);
+    v.set("picture", p.picture);
+    v.set("locale", p.locale);
+    v.set("deleted_at", p.deleted_at);
+    v.set("version", p.version);
     v.set("updated_at", p.updated_at);
     ind = i_ok;
   }
--- lavender/src/portal-dao.cpp
+++ lavender/src/portal-dao.cpp
@@ -1,5 +1,146 @@
 #include "palm/portal.hpp"
 
+// ----------------------------------------------------------------------------
+
+boost::optional<palm::portal::dao::users::Item> palm::portal::dao::users::get(
+    soci::session& db, int id) {
+  boost::optional<palm::portal::dao::users::Item> it;
+  db << R"SQL(SELECT id, uid, lang, timezone, sign_in_count, current_sign_in_at, current_sign_in_ip, last_sign_in_at, last_sign_in_ip, locked_at, deleted_at, version, updated_at FROM users WHERE id = :id LIMIT 1)SQL",
+      soci::use(id, "id"), soci::into(it);
+  return it;
+}
+boost::optional<palm::portal::dao::users::Item> palm::portal::dao::users::get(
+    soci::session& db, const std::string& uid) {
+  boost::optional<palm::portal::dao::users::Item> it;
+  db << R"SQL(SELECT id, uid, lang, timezone, sign_in_count, current_sign_in_at, current_sign_in_ip, last_sign_in_at, last_sign_in_ip, locked_at, deleted_at, version, updated_at FROM users WHERE uid = :uid LIMIT 1)SQL",
+      soci::use(uid, "uid"), soci::into(it);
+  return it;
+}
+void palm::portal::dao::users::enable(soci::session& db, int id) {
+  db << R"SQL(UPDATE users SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+void palm::portal::dao::users::disable(soci::session& db, int id) {
+  db << R"SQL(UPDATE users SET deleted_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+// ----------------------------------------------------------------------------
+
+boost::optional<palm::portal::dao::users::email::Item>
+palm::portal::dao::users::email::get(soci::session& db, int id) {
+  boost::optional<palm::portal::dao::users::email::Item> it;
+  db << R"SQL(SELECT id, user_id, real_name, email, password, avatar, confirmed_at, deleted_at, version, updated_at FROM email_users WHERE id = :id LIMIT 1)SQL",
+      soci::use(id, "id"), soci::into(it);
+  return it;
+}
+boost::optional<palm::portal::dao::users::email::Item>
+palm::portal::dao::users::email::get(soci::session& db,
+                                     const std::string& email) {
+  boost::optional<palm::portal::dao::users::email::Item> it;
+  db << R"SQL(SELECT id, user_id, real_name, email, password, avatar, confirmed_at, deleted_at, version, updated_at FROM email_users WHERE email = :email LIMIT 1)SQL",
+      soci::use(email, "email"), soci::into(it);
+  return it;
+}
+void palm::portal::dao::users::email::enable(soci::session& db, int id) {
+  db << R"SQL(UPDATE email_users SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+void palm::portal::dao::users::email::disable(soci::session& db, int id) {
+  db << R"SQL(UPDATE email_users SET deleted_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+// ----------------------------------------------------------------------------
+
+boost::optional<palm::portal::dao::users::wechat::mini_program::Item>
+palm::portal::dao::users::wechat::mini_program::get(soci::session& db, int id) {
+  boost::optional<palm::portal::dao::users::wechat::mini_program::Item> it;
+  db << R"SQL(SELECT id, user_id, union_id, app_id, open_id, nickname, avatar_url, deleted_at, version, updated_at FROM wechat_mini_program_users WHERE id = :id LIMIT 1)SQL",
+      soci::use(id, "id"), soci::into(it);
+  return it;
+}
+void palm::portal::dao::users::wechat::mini_program::enable(soci::session& db,
+                                                            int id) {
+  db << R"SQL(UPDATE wechat_mini_program_users SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+void palm::portal::dao::users::wechat::mini_program::disable(soci::session& db,
+                                                             int id) {
+  db << R"SQL(UPDATE wechat_mini_program_users SET deleted_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+boost::optional<palm::portal::dao::users::wechat::mini_program::Item>
+palm::portal::dao::users::wechat::mini_program::get(
+    soci::session& db, const std::string& union_id) {
+  boost::optional<palm::portal::dao::users::wechat::mini_program::Item> it;
+  db << R"SQL(SELECT id, user_id, union_id, app_id, open_id, nickname, avatar_url, deleted_at, version, updated_at FROM wechat_mini_program_users WHERE union_id = :union_id LIMIT 1)SQL",
+      soci::use(union_id, "union_id"), soci::into(it);
+  return it;
+}
+boost::optional<palm::portal::dao::users::wechat::mini_program::Item>
+palm::portal::dao::users::wechat::mini_program::get(
+    soci::session& db, const std::string& app_id, const std::string& open_id) {
+  boost::optional<palm::portal::dao::users::wechat::mini_program::Item> it;
+  db << R"SQL(SELECT id, user_id, union_id, app_id, open_id, nickname, avatar_url, deleted_at, version, updated_at FROM wechat_mini_program_users WHERE app_id = :app_id AND open_id = :open_id LIMIT 1)SQL",
+      soci::use(app_id, "app_id"), soci::use(open_id, "open_id"),
+      soci::into(it);
+  return it;
+}
+// ----------------------------------------------------------------------------
+
+boost::optional<palm::portal::dao::users::wechat::oauth2::Item>
+palm::portal::dao::users::wechat::oauth2::get(soci::session& db, int id) {
+  boost::optional<palm::portal::dao::users::wechat::oauth2::Item> it;
+  db << R"SQL(SELECT id, user_id, union_id, app_id, open_id, nickname, sex, city, province, country, head_img_url, privilege, lang, deleted_at, version, updated_at FROM wechat_oauth2_users WHERE id = :id LIMIT 1)SQL",
+      soci::use(id, "id"), soci::into(it);
+  return it;
+}
+boost::optional<palm::portal::dao::users::wechat::oauth2::Item>
+palm::portal::dao::users::wechat::oauth2::get(soci::session& db,
+                                              const std::string& union_id) {
+  boost::optional<palm::portal::dao::users::wechat::oauth2::Item> it;
+  db << R"SQL(SELECT id, user_id, union_id, app_id, open_id, nickname, sex, city, province, country, head_img_url, privilege, lang, deleted_at, version, updated_at FROM wechat_oauth2_users WHERE union_id = :union_id LIMIT 1)SQL",
+      soci::use(union_id, "union_id"), soci::into(it);
+  return it;
+}
+boost::optional<palm::portal::dao::users::wechat::oauth2::Item>
+palm::portal::dao::users::wechat::oauth2::get(soci::session& db,
+                                              const std::string& app_id,
+                                              const std::string& open_id) {
+  boost::optional<palm::portal::dao::users::wechat::oauth2::Item> it;
+  db << R"SQL(SELECT id, user_id, union_id, app_id, open_id, nickname, sex, city, province, country, head_img_url, privilege, lang, deleted_at, version, updated_at FROM wechat_oauth2_users WHERE app_id = :app_id AND open_id = :open_id LIMIT 1)SQL",
+      soci::use(app_id, "app_id"), soci::use(open_id, "open_id"),
+      soci::into(it);
+  return it;
+}
+void palm::portal::dao::users::wechat::oauth2::enable(soci::session& db,
+                                                      int id) {
+  db << R"SQL(UPDATE wechat_oauth2_users SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+void palm::portal::dao::users::wechat::oauth2::disable(soci::session& db,
+                                                       int id) {
+  db << R"SQL(UPDATE wechat_oauth2_users SET deleted_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+// ----------------------------------------------------------------------------
+boost::optional<palm::portal::dao::users::google::oauth2::Item>
+palm::portal::dao::users::google::oauth2::get(soci::session& db, int id) {
+  boost::optional<palm::portal::dao::users::google::oauth2::Item> it;
+  db << R"SQL(SELECT id, user_id, subject, email, email_verified, name, picture, locale, deleted_at, version, updated_at FROM google_oauth2_users WHERE id = :id LIMIT 1)SQL",
+      soci::use(id, "id"), soci::into(it);
+  return it;
+}
+void palm::portal::dao::users::google::oauth2::enable(soci::session& db,
+                                                      int id) {
+  db << R"SQL(UPDATE google_oauth2_users SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+void palm::portal::dao::users::google::oauth2::disable(soci::session& db,
+                                                       int id) {
+  db << R"SQL(UPDATE google_oauth2_users SET deleted_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
+      soci::use(id, "id");
+}
+// ----------------------------------------------------------------------------
 std::vector<std::string> palm::portal::dao::locales::languages(
     soci::session& db) {
   std::vector<std::string> items;
@@ -18,6 +159,9 @@ void palm::portal::dao::locales::update(soci::session& db, int id,
                                         const std::string& message) {
   db << R"SQL(UPDATE locales SET message = :message, updated_at = CURRENT_TIMESTAMP WHERE id = :id)SQL",
       soci::use(id, "id"), soci::use(message, "message");
+}
+void palm::portal::dao::locales::destroy(soci::session& db, int id) {
+  db << R"SQL(DELETE FROM locales WHERE id = :id)SQL", soci::use(id, "id");
 }
 int palm::portal::dao::locales::count(soci::session& db) {
   int c;
@@ -55,3 +199,5 @@ palm::portal::dao::locales::get(soci::session& db, const std::string& lang,
       soci::use(lang, "lang"), soci::use(code, "code"), soci::into(it);
   return it;
 }
+
+// ----------------------------------------------------------------------------
