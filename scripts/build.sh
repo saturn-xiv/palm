6ae6031aee955caa5b9bafff19105001f0d22312
Jeremy Zheng <jitang.zheng@gmail.com>
2025-07-14 03:14:17 +0000

    :package: add build-deb support

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,20 +10,83 @@ export BOOST_FLAGS=""
 export WORK_DIR=$PWD
 
 function build_on_arch() {
+    cd $WORK_DIR/
+
     cmake --preset=arch -DVCPKG_TARGET_TRIPLET=x64-linux-release -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORK_DIR/toolchains/arch/clang.cmake $BOOST_FLAGS $THRIFT_FLAGS $CASBIN_FLAGS
     cmake --build $WORK_DIR/build/arch
 }
 
 function build_x86_64_on_ubuntu() {
-    cmake --preset=x86_64 -DVCPKG_TARGET_TRIPLET=x64-linux-release $BOOST_FLAGS $THRIFT_FLAGS $CASBIN_FLAGS
-    cmake --build $WORK_DIR/build/x86_64
+    cd $WORK_DIR/
+    # cmake --preset=x86_64 -DVCPKG_TARGET_TRIPLET=x64-linux-release $BOOST_FLAGS $THRIFT_FLAGS $CASBIN_FLAGS
+    # cmake --build $WORK_DIR/build/x86_64
+
     # cmake --preset=x86_64 -DVCPKG_TARGET_TRIPLET=x64-linux-release -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORK_DIR/toolchains/ubuntu/clang/x86_64.cmake $THRIFT_FLAGS
     # cmake --build $WORK_DIR/build/x86_64
+
+    build_deb x86_64 amd64
 }
 
 function build_aarch64_on_ubuntu() {
-    cmake --preset=aarch64 -DVCPKG_TARGET_TRIPLET=arm64-linux-release -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORK_DIR/toolchains/ubuntu/gcc/aarch64.cmake $BOOST_FLAGS $THRIFT_FLAGS $CASBIN_FLAGS
-    cmake --build $WORK_DIR/build/aarch64
+    # cmake --preset=aarch64 -DVCPKG_TARGET_TRIPLET=arm64-linux-release -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORK_DIR/toolchains/ubuntu/gcc/aarch64.cmake $BOOST_FLAGS $THRIFT_FLAGS $CASBIN_FLAGS
+    # cmake --build $WORK_DIR/build/aarch64
+
+    build_deb aarch64 arm64
+}
+
+function build_deb() {
+    local target=$WORK_DIR/tmp/$VERSION_CODENAME-$1/palm
+    local version=$(date +"%Y.%-m-%-d")
+    if [ -d $target ]; then
+        rm -rf $target
+    fi
+    mkdir -p $target/DEBIAN
+    cat >$target/DEBIAN/control <<EOF
+Package: palm
+Version: $version
+Maintainer: Jeremy
+Architecture: $2
+Description: A total free education and translation solution
+Homepage: https://github.com/saturn-xiv/palm
+EOF
+
+    mkdir -p $target/etc/palm
+
+    mkdir -p $target/usr/bin
+    cd $WORK_DIR/build/$1/
+    cp -v lavender/lavender bamboo/bamboo phlox/phlox $target/usr/bin/
+
+    mkdir -p $target/usr/share/palm/
+    cd $WORK_DIR
+    cp -rv README.md LICENSE $target/usr/share/palm/
+
+    mkdir -p $target/var/lib/palm/lavender
+    cd $WORK_DIR/lavender/
+    cp -rv locales vendors $target/var/lib/palm/lavender/
+    mkdir -p $target/var/lib/palm/lavender/db
+    cd $WORK_DIR/lavender/db
+    cp -rv README.md migrations $target/var/lib/palm/lavender/db/
+
+    cd $(dirname $target)
+    # dpkg-deb -x xxx.deb xxx
+    # dpkg-deb -I xxx.deb
+    dpkg-deb -b palm palm-$2-$version-$VERSION_CODENAME.deb
+}
+
+function build_dashboard() {
+    echo "build dashboard($1)"
+    cd $WORK_DIR/$1
+    if [ ! -d node_modules ]; then
+        npm install
+    fi
+    npm run build
+}
+
+function build_assets() {
+    cd $WORK_DIR/
+    if [ ! -d node_modules ]; then
+        npm install
+    fi
 }
 
 . /etc/os-release
