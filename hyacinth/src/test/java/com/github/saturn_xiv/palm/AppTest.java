ac33e9687b60c6f2463f088a3e9ee10cf0f9e0bf
Jeremy Zheng <jitang.zheng@gmail.com>
2025-08-15 02:24:18 +0000

    :tada: create hyacinth project

---
+++ hyacinth/src/test/java/com/github/saturn_xiv/palm/AppTest.java
@@ -0,0 +1,19 @@
+package com.github.saturn_xiv.palm;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
