bb5b2a15847eaf3c42cc5b6f2bc043e6684b9785
Jeremy Zheng <jitang.zheng@gmail.com>
2025-08-15 06:50:06 +0000

    :wrench: add logback support

--- hyacinth/README.md
+++ hyacinth/README.md
@@ -2,5 +2,6 @@
 
 ```bash
 mvn clean && mvn package
-java -jar target/hyacinth-2025.8.15-jar-with-dependencies.jar -d -p 8080 -c config.toml
+java -Dlogback.configurationFile=logback.xml -jar target/hyacinth-2025.8.15-jar-with-dependencies.jar -p 8080 -c config.toml
+java -cp 'target/hyacinth-2025.8.15-jar-with-dependencies.jar:libs/*' com.github.saturn_xiv.palm.hyacinth.App -p 8080 -c config.toml
 ```
--- hyacinth/src/main/java/com/github/saturn_xiv/palm/hyacinth/App.java
+++ hyacinth/src/main/java/com/github/saturn_xiv/palm/hyacinth/App.java
@@ -3,6 +3,8 @@ package com.github.saturn_xiv.palm.hyacinth;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 
 import com.github.saturn_xiv.palm.hyacinth.models.Config;
@@ -10,7 +12,11 @@ import com.github.saturn_xiv.palm.hyacinth.models.Config;
 public class App {
     public static void main(String[] args) throws IOException {
         TomlMapper mapper = new TomlMapper();
-        Config config = mapper.readValue(new File("config.toml"), Config.class);
-        System.out.println("Hello World!" + config.backend().host());
+        String config_file = "config.toml";
+        logger.info("load configuration from {}", config_file);
+        Config config = mapper.readValue(new File(config_file), Config.class);
+        logger.debug("open http://{}:{}", config.backend().host(), config.backend().port());
     }
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 }
