b40887c90db7324393089ed15c196cd0771d857f
Jeremy Zheng <jitang.zheng@gmail.com>
2025-08-18 15:20:44 +0000

    :construction: add currencies import

--- jasmine/services/portal/models/iso4217.go
+++ jasmine/services/portal/models/iso4217.go
@@ -1,25 +1,14 @@
 package models
 
 import (
+	_ "embed"
+	"encoding/xml"
 	"log/slog"
+	"strconv"
 	"time"
 
 	"gorm.io/gorm"
 )
-
-// struct Currency {
-//   int id;
-//   std::string name;
-//   std::string code;
-//   std::string number;
-//   std::string country;
-//   boost::optional<int> units;
-//   boost::optional<int> is_fund;
-// };
-
-// type Currency struct{
-
-// }
 
 // https://www.iso.org/iso-4217-currency-codes.html
 type Currency struct {
@@ -28,8 +17,8 @@ type Currency struct {
 	Code      string
 	Number    string
 	Country   string
-	Units     *uint8
-	Fund      *bool
+	Units     *uint32
+	IsFund      *bool
 	CreatedAt time.Time
 }
 
@@ -37,34 +26,88 @@ func (Currency) TableName() string {
 	return "currencies"
 }
 
+type iso_4217 struct {
+	XMLName xml.Name `xml:"ISO_4217"`
+	Pblshd  string   `xml:"Pblshd,attr"`
+	CcyTbl  CcyTbl   `xml:"CcyTbl"`
+}
+type CcyTbl struct {
+	CcyNtry []CcyNtry `xml:"CcyNtry"`
+}
+type CcyNtry struct {
+	CtryNm     string  `xml:"CtryNm"`
+	CcyNm      CcyNm   `xml:"CcyNm"`
+	Ccy        string  `xml:"Ccy"`
+	CcyNbr     string  `xml:"CcyNbr"`
+	CcyMnrUnts *string `xml:"CcyMnrUnts"`
+}
+type CcyNm struct {
+	IsFund *bool  `xml:"IsFund,attr"`
+	Text   string `xml:",chardata"`
+}
+
 type currencyItem struct {
 	name    string
 	code    string
 	number  string
 	country string
-	units   *uint8
+	units   *int
 	fund    *bool
 }
 
 func LoadIso4217ListOne(db *gorm.DB) (int, int, error) {
-
 	items, err := load_currencies_from_iso4317_list_one()
 	if err != nil {
 		return 0, 0, err
 	}
 	inserted := 0
-	now := time.Now()
 	for _, it := range items {
 		slog.Debug("create", slog.String("name", it.name), slog.String("code", it.code), slog.String("country", it.country))
-		db.Create(&Currency{Name: it.name, Code: it.code, Number: it.number, Country: it.country, Units: it.units, Fund: it.fund, CreatedAt: now})
+		var units *uint32
+		if it.units != nil {
+			v := uint32(*it.units)
+			units = &v
+		}
+		if err = db.Create(&Currency{Name: it.name, Code: it.code, Number: it.number, Country: it.country, Units: units, IsFund: it.fund}).Error; err != nil {
+			return len(items), 0, nil
+		}
 		inserted = inserted + 1
 	}
 	return len(items), inserted, nil
 }
 
+//go:embed iso4217/list-one.xml
+var iso4217_list_one_xml []byte
+
 func load_currencies_from_iso4317_list_one() ([]currencyItem, error) {
 	slog.Debug("load currencies from ISO4217/list-one.xml")
+
+	var root iso_4217
+	if err := xml.Unmarshal(iso4217_list_one_xml, &root); err != nil {
+		return nil, err
+	}
 	items := make([]currencyItem, 0)
+	for _, it := range root.CcyTbl.CcyNtry {
+		if it.CcyMnrUnts == nil {
+			continue
+		}
+		jt := currencyItem{
+			name:    it.CcyNm.Text,
+			code:    it.Ccy,
+			country: it.CtryNm,
+			number:  it.CcyNbr,
+			fund:    it.CcyNm.IsFund,
+		}
+		if *it.CcyMnrUnts != "N.A." {
+			units, err := strconv.Atoi(*it.CcyMnrUnts)
+			if err != nil {
+				return nil, err
+			}
+			jt.units = &units
+		}
+
+		items = append(items, jt)
+	}
 	return items, nil
 
 }
