6db1672133ac309aa78fd1337d4e3d9e13c551e6
Jeremy Zheng <jitang.zheng@gmail.com>
2025-09-02 08:50:26 +0000

    :bug: fix package dashboard

--- scripts/dashboard/pack.sh
+++ scripts/dashboard/pack.sh
@@ -4,6 +4,6 @@ set -e
 
 export PACKAGE="node_modules-$(date +"%Y%m%d%H%M%S").tar.xz"
 
-XZ_OPT=-9 tar cJf $PACKAGE node_modules package.json package-lock.json
+XZ_OPT=-9 tar -cJf $PACKAGE node_modules package.json package-lock.json
 
 echo "done($PACKAGE)."
