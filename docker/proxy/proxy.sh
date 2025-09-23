2ac7c90a29860a794157db33140f3487fb2de5d4
Jeremy Zheng <jitang.zheng@gmail.com>
2025-09-23 04:55:43 +0000

    :bug: remove php-spell

--- docker/proxy/proxy.sh
+++ docker/proxy/proxy.sh
@@ -5,4 +5,4 @@ set -e
 source $HOME/local/python3/bin/activate
 source /workspace/.env
 
-pproxy --reuse -l "$PROXY_URI" -r "ssh://$SSH_HOST:$SSH_PORT#$SSH_USER::/workspace/$SSH_KEYFILE"
+pproxy -a 30 --reuse -l "$PROXY_URI" -r "ssh://$SSH_HOST:$SSH_PORT#$SSH_USER::/workspace/$SSH_KEYFILE"
