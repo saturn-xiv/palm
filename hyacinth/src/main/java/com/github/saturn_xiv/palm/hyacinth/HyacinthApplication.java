2e2317b58e2642df2dec773f5879a671c62a19b0
Jeremy Zheng <jitang.zheng@gmail.com>
2025-09-24 22:31:43 +0000

    :tada: create hyacinth project

---
+++ hyacinth/src/main/java/com/github/saturn_xiv/palm/hyacinth/HyacinthApplication.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.hyacinth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyacinthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyacinthApplication.class, args);
+	}
+
+}
