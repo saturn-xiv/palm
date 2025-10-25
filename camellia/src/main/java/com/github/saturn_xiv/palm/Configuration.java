032363d8bdc6ee30b3597e5eefaecdfe12ce80ab
Jeremy Zheng <jitang.zheng@gmail.com>
2025-10-25 13:48:39 +0000

    :tada: use spring boot for camellia project

--- camellia/src/main/java/com/github/saturn_xiv/palm/Configuration.java
+++
@@ -1,9 +0,0 @@
-package com.github.saturn_xiv.palm;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import jakarta.validation.constraints.*;
-
-public class Configuration extends io.dropwizard.core.Configuration {
-    // TODO: implement service configuration
-}
