0bb6ab5f5bf3e1bfb9bc1db53a690f935106d432
Jeremy Zheng <jitang.zheng@gmail.com>
2025-10-31 02:07:36 +0000

    :bento: merege bootstrap assets

--- bamboo/mix.exs
+++ bamboo/mix.exs
@@ -4,7 +4,7 @@ defmodule Bamboo.MixProject do
   def project do
     [
       app: :bamboo,
-      version: "0.1.0",
+      version: "2025.10.31",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
