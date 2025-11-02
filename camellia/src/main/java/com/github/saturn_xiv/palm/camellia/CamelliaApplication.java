7549d63d903bde99fe2ac2799c12d4f1862b23a6
Jeremy Zheng <jitang.zheng@gmail.com>
2025-11-02 09:24:47 +0000

    :tada: create camellia project

---
+++ camellia/src/main/java/com/github/saturn_xiv/palm/camellia/CamelliaApplication.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.camellia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelliaApplication.class, args);
+	}
+
+}
---
+++ camellia/src/test/java/com/github/saturn_xiv/palm/camellia/CamelliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.camellia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
