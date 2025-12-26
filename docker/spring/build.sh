cd3a47a52424dc5ce8e16cba5705471d28c387d1
Jeremy Zheng <jitang.zheng@gmail.com>
2025-12-26 13:05:36 +0000

    :bricks: use docker instead of podman

--- docker/spring/build.sh
+++ docker/spring/build.sh
@@ -5,16 +5,20 @@ set -e
 export VERSION=$(date "+%4Y%m%d%H%M%S")
 export CODE="palm-spring"
 
-podman pull archlinux:latest
-podman build -t $CODE .
-podman save --format=oci-archive -o $CODE-$VERSION.tar $CODE
+# podman pull archlinux:latest
+# podman build -t $CODE .
+# podman save --format=oci-archive -o $CODE-$VERSION.tar $CODE
 
 # xz -z -C sha256 --best -T 1 $TAR.tar
 # md5sum $TAR.tar.xz >>$TAR.md5
 # split -d -b 1G $CODE-$VERSION.tar.xz $CODE-$VERSION.tar.xz.
 # md5sum $CODE-$VERSION.tar.xz* >>$CODE-$VERSION.md5
+# split -d -b 1G $CODE-$VERSION.tar $CODE-$VERSION.tar.
 
-split -d -b 1G $CODE-$VERSION.tar $CODE-$VERSION.tar.
+
+docker pull archlinux:latest
+docker build -t $CODE .
+docker save -o $CODE-$VERSION.tar $CODE
 md5sum $CODE-$VERSION.tar* >>$CODE-$VERSION.md5
 
 # cat $CODE-$VERSION.tar.?? >$CODE-$VERSION.tar
