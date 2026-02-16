69dd4d1f22bcbdc3ee998839782d5b6fd5e256f5
Jeremy Zheng <jitang.zheng@gmail.com>
2026-02-16 07:13:50 +0000

    :wrench: use mold linker

---
+++ tulip/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+declare -a targets=("x86_64" "aarch64" "riscv64")
+for i in "${targets[@]}"
+do
+   cmake --preset=$i
+   cmake --build build/$i
+done
+
+
+echo 'done.'
+exit 0
