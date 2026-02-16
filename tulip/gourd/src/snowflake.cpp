0561af05ee63154392f7f60aec246ac3cd89112a
Jeremy Zheng <jitang.zheng@gmail.com>
2026-02-16 07:06:26 +0000

    :wrench: add clang toolchain support

---
+++ tulip/gourd/src/snowflake.cpp
@@ -0,0 +1,1 @@
+#include "palm/snowflake.hpp"
--- tulip/tests/utils.cpp
+++ tulip/tests/utils.cpp
@@ -1,6 +1,7 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
+#include "palm/snowflake.hpp"
 #include "palm/utils.hpp"
 
 TEST_CASE("random", "[utils]") {
@@ -8,3 +9,5 @@ TEST_CASE("random", "[utils]") {
   SECTION("uuid") { REQUIRE(1 + 1 == 2); }
   SECTION("alphanumeric") { REQUIRE(1 + 1 == 2); }
 }
+
+TEST_CASE("Generate snowflake ids", "[snowflake]") {}
