c04ad317b58be7c49fe2515112282b21b086e90c
Jeremy Zheng <jitang.zheng@gmail.com>
2026-03-31 02:19:27 +0000

    :construction: add rabbitmq watcher for casbin

--- phlox/src/casbin/mod.rs
+++ phlox/src/casbin/mod.rs
@@ -1,7 +1,8 @@
-use std::{process, sync::Arc};
+use std::{ops::Deref, process, sync::Arc};
 
 use diesel_adapter::{DieselAdapter, casbin::prelude::*};
 use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
 use tokio::{
     runtime::Handle,
     sync::Mutex,
@@ -9,16 +10,20 @@ use tokio::{
     time::{Duration, sleep},
 };
 
+pub use casbin::{CoreApi, Enforcer, RbacApi};
+
 use super::{
-    Error, HttpError, Result,
+    HttpError, Result,
     orm::postgresql::Pool as PostgreSqlPool,
     queue::{
-        Consumer,
+        FlexbuffersConsumer,
         rabbitmq::{
             BasicPublishOptions, Client as RabbitMqClient, ExchangeDeclareOptions, ExchangeKind,
+            FlexBuffersMessage, FlexbuffersConsumer as RabbitMqFlexbuffersConsumer,
             QueueBindOptions, QueueDeclareOptions,
         },
     },
+    random::uuid,
 };
 
 // https://casbin.apache.org/
@@ -26,10 +31,12 @@ pub async fn postgresql_rabbitmq_enforcer(
     db: PostgreSqlPool,
     queue: Arc<RabbitMqClient>,
 ) -> Result<Arc<Mutex<Enforcer>>> {
+    let id = uuid();
+    log::info!("open casbin enforcer {}", id);
     let enforcer = Arc::new(Mutex::new({
         let model = DefaultModel::from_str(RBAC_MODEL).await?;
         let adapter = DieselAdapter::with_pool(db)?;
-        let watcher = RabbitMqWatcher::new(queue.clone(), QUEUE).await?;
+        let watcher = RabbitMqWatcher::new(&id, queue.clone(), QUEUE).await?;
         let mut it = Enforcer::new(model, adapter).await?;
         it.set_watcher(Box::new(watcher));
         it
@@ -37,8 +44,11 @@ pub async fn postgresql_rabbitmq_enforcer(
     {
         let pid = process::id();
         let client = queue.clone();
-        let consumer = RabbitMqWatcherConsumer {
-            enforcer: enforcer.clone(),
+        let consumer = RabbitMqFlexbuffersConsumer {
+            handler: RabbitMqWatcherConsumer {
+                id: id.clone(),
+                enforcer: enforcer.clone(),
+            },
         };
         let queue = client
             .declare_anonymous_queue(QueueDeclareOptions {
@@ -50,7 +60,6 @@ pub async fn postgresql_rabbitmq_enforcer(
         client
             .bind_queue(&queue, QUEUE, "*", QueueBindOptions::default())
             .await?;
-
         spawn(async move {
             loop {
                 if let Err(e) = client
@@ -70,12 +79,14 @@ pub const RBAC_MODEL: &str = include_str!("rbac_model.conf");
 pub const QUEUE: &str = "casbin.watcher";
 
 pub struct RabbitMqWatcher {
-    pub client: Arc<RabbitMqClient>,
-    pub exchange: String,
+    client: Arc<RabbitMqClient>,
+    exchange: String,
+    id: String,
 }
 
 impl RabbitMqWatcher {
-    pub async fn new(client: Arc<RabbitMqClient>, exchange: &str) -> Result<Self> {
+    pub async fn new(id: &str, client: Arc<RabbitMqClient>, exchange: &str) -> Result<Self> {
+        log::debug!("open casbin watcher for {}", id);
         client
             .declare_exchange(
                 exchange,
@@ -85,6 +96,7 @@ impl RabbitMqWatcher {
             .await
             .map_err(|e| HttpError(StatusCode::INTERNAL_SERVER_ERROR, Some(e.to_string())))?;
         Ok(Self {
+            id: id.to_string(),
             client,
             exchange: exchange.to_string(),
         })
@@ -93,23 +105,24 @@ impl RabbitMqWatcher {
 
 impl Watcher for RabbitMqWatcher {
     fn set_update_callback(&mut self, _cb: Box<dyn FnMut(String) + Send + Sync>) {
-        log::debug!("set casbin-watcher callback");
+        log::debug!("set casbin-watcher({}) callback", self.id);
     }
     fn update(&mut self, event: casbin::EventData) {
-        let event = event.to_string();
-        let content_type = mime::TEXT_PLAIN_UTF_8.to_string();
+        log::debug!("casbin event({}): {}", self.id, event);
+
         let handle = Handle::current();
-        handle.block_on(async {
-            if let Err(e) = self
-                .client
-                .publish(
-                    &self.exchange,
-                    "",
-                    &content_type,
-                    event.as_bytes(),
-                    BasicPublishOptions::default(),
-                )
-                .await
+        let id = self.id.clone();
+        let client = self.client.clone();
+        let exchange = self.exchange.clone();
+        handle.spawn(async move {
+            if let Err(e) = FlexBuffersMessage::publish(
+                client.deref(),
+                &exchange,
+                "",
+                &WatcherMessage { id },
+                BasicPublishOptions::default(),
+            )
+            .await
             {
                 log::error!("{}", e);
             }
@@ -118,14 +131,22 @@ impl Watcher for RabbitMqWatcher {
 }
 
 pub struct RabbitMqWatcherConsumer {
+    pub id: String,
     pub enforcer: Arc<Mutex<Enforcer>>,
 }
 
-impl Consumer for RabbitMqWatcherConsumer {
-    type Error = Error;
-    async fn consume(&self, _id: &str, _content_type: &str, _payload: &[u8]) -> Result<()> {
-        let mut it = self.enforcer.lock().await;
-        it.load_policy().await?;
+impl FlexbuffersConsumer for RabbitMqWatcherConsumer {
+    type Message = WatcherMessage;
+    async fn consume(&self, _id: &str, task: &Self::Message) -> Result<()> {
+        if task.id != self.id {
+            let mut it = self.enforcer.lock().await;
+            it.load_policy().await?;
+        }
         Ok(())
     }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WatcherMessage {
+    pub id: String,
 }
---
+++ phlox/tests/casbin_test.rs
@@ -0,0 +1,61 @@
+use std::sync::Arc;
+
+use phlox::{
+    Result,
+    casbin::{Enforcer, RbacApi, postgresql_rabbitmq_enforcer},
+    orm::postgresql::Node as PostgreSql,
+    queue::rabbitmq::Node as RabbitMq,
+};
+use tokio::{
+    sync::Mutex,
+    time::{Duration, sleep},
+};
+
+const USER: &str = "u.bbb";
+const ROLE: &str = "r.admin";
+
+#[tokio::test]
+async fn watcher() {
+    let enforcer_a = open_enforcer().await.unwrap();
+    let enforcer_b = open_enforcer().await.unwrap();
+
+    {
+        let mut enf = enforcer_a.lock().await;
+        enf.delete_role(ROLE).await.unwrap();
+        assert!(!enf.has_role_for_user(USER, ROLE, None));
+    }
+    sleep(Duration::from_secs(1)).await;
+    {
+        let enf = enforcer_b.lock().await;
+        assert!(!enf.has_role_for_user(USER, ROLE, None));
+    }
+    sleep(Duration::from_secs(1)).await;
+    {
+        let mut enf = enforcer_a.lock().await;
+        enf.add_role_for_user(USER, ROLE, None).await.unwrap();
+    }
+    sleep(Duration::from_secs(1)).await;
+    {
+        let enf = enforcer_b.lock().await;
+        assert!(enf.has_role_for_user(USER, ROLE, None));
+    }
+}
+
+async fn open_enforcer() -> Result<Arc<Mutex<Enforcer>>> {
+    let db = PostgreSql {
+        host: "127.0.0.1".to_string(),
+        port: 5432,
+        user: "www".to_string(),
+        password: Some("change-me".to_string()),
+        db_name: "phlox_testing".to_string(),
+        pool_size: 16,
+    };
+    let queue = RabbitMq {
+        host: "127.0.0.1".to_string(),
+        port: 5672,
+        user: "www".to_string(),
+        password: "change-me".to_string(),
+        virtual_host: "testing".to_string(),
+    };
+    postgresql_rabbitmq_enforcer(db.open()?, queue.open().await?).await
+}
