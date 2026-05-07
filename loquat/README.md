de0a5ac35506e2dc9ab5c12965718228b0f744f9
Jeremy Zheng <jitang.zheng@gmail.com>
2026-05-07 05:58:25 +0000

    :bug: fix build on noble

--- loquat/README.md
+++ loquat/README.md
@@ -1,7 +1,7 @@
 # Usage
 
 ```bash
-$ podman run --rm -it --events-backend=file --network host -v $(dirname $PWD):/workspace:z ubuntu:jammy
+$ podman run --rm -it --events-backend=file --network host -v $(dirname $PWD):/workspace:z ubuntu:noble
 > cd /workspace/loquat/
 > ./build.sh
 ```
--- loquat/build.sh
+++ loquat/build.sh
@@ -5,23 +5,22 @@ set -e
 . /etc/os-release
 
 # https://github.com/grpc/grpc/blob/master/BUILDING.md#pre-requisites
-if [[ "$ID" == "ubuntu" ]]; then
-    apt update
-    apt -y upgrade
-    DEBIAN_FRONTEND=noninteractive apt install -y curl git zip \
-        pkg-config build-essential g++-12 cmake ninja-build flex bison \
-        linux-libc-dev libssl-dev libevent-dev libboost-all-dev
-else
+if [[ "$ID" != "ubuntu" ]]; then
     echo "Unsupported system: $ID/$VERSION_CODENAME"
     exit 1
 fi
+apt update
+apt -y upgrade
+DEBIAN_FRONTEND=noninteractive apt install -y curl git zip \
+    pkg-config build-essential cmake ninja-build flex bison \
+    linux-libc-dev libssl-dev libevent-dev libboost-all-dev
 
 export SOURCE_ROOT=$PWD
 export BUILD_ROOT=$PWD/build/Release-$VERSION_CODENAME-$(uname -m)
 
 mkdir -p $BUILD_ROOT
-# -Wno-dev
-CC=gcc-12 CXX=g++-12 cmake -DCMAKE_BUILD_TYPE=Release \
+# https://thrift.apache.org/lib/cpp.html
+cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=OFF -DBoost_USE_STATIC_LIBS=ON -DBUILD_COMPILER=OFF -DWITH_LIBEVENT=ON -DBUILD_CPP=ON -DWITH_JAVA=OFF -DWITH_PYTHON=OFF -DWITH_NODEJS=OFF -DWITH_JAVASCRIPT=OFF \
     -DABSL_PROPAGATE_CXX_STD=ON \
     -DTINK_USE_SYSTEM_OPENSSL=ON -DTINK_BUILD_TESTS=OFF \
