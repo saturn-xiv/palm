8f6ef9e7b3b111882a6bedbfcf15ab6cb980bd4a
Jeremy Zheng <jitang.zheng@gmail.com>
2026-05-10 03:32:21 +0000

    :construction: add casbin rbac service

--- dahlia/src/dahlia/rbac/server.py
+++ dahlia/src/dahlia/rbac/server.py
@@ -1,8 +1,9 @@
 import logging
 
 from google.protobuf.empty_pb2 import Empty
+from grpc import StatusCode
 
-from dahlia.protocols import rbac_pb2_grpc, rbac_pb2, from_str, to_str, role_from_str, user_from_str
+from dahlia.protocols import rbac_pb2_grpc, rbac_pb2, from_str, to_str, role_from_str, user_from_str, permission_from_line
 
 logger = logging.getLogger(__name__)
 
@@ -56,49 +57,76 @@ class Server(rbac_pb2_grpc.EnforcerServicer):
         return res
 
     def GetUsersForRole(self, request, context):
-        # TODO
-        return rbac_pb2.UsersResponse()
+        res = rbac_pb2.UsersResponse()
+        subject = rbac_pb2.Subject(role=request)
+        for it in self.enforcer.get_users_for_role(to_str(subject)):
+            usr = user_from_str(it)
+            res.items.extend([usr])
+        return res
+
+    def GetImplicitUsersForRole(self, request, context):
+        res = rbac_pb2.UsersResponse()
+        subject = rbac_pb2.Subject(role=request)
+        for it in self.enforcer.get_implicit_users_for_role(to_str(subject)):
+            usr = user_from_str(it)
+            res.items.extend([usr])
+        return res
 
     def HasRoleForUser(self, request, context):
-        # TODO
+        usr = rbac_pb2.Subject(user=request.user)
+        rol = rbac_pb2.Subject(role=request.role)
+        if not self.enforcer.has_role_for_user(to_str(usr), to_str(rol)):
+            context.abort(StatusCode.NOT_FOUND, "")
         return Empty()
 
     def AddRoleForUser(self, request, context):
-        # TODO
+        usr = rbac_pb2.Subject(user=request.user)
+        rol = rbac_pb2.Subject(role=request.role)
+        self.enforcer.add_role_for_user(to_str(usr), to_str(rol))
         return Empty()
 
     def DeleteRoleForUser(self, request, context):
-        # TODO
+        usr = rbac_pb2.Subject(user=request.user)
+        rol = rbac_pb2.Subject(role=request.role)
+        self.enforcer.delete_role_for_user(to_str(usr), to_str(rol))
         return Empty()
 
     def DeleteRole(self, request, context):
-        # TODO
+        rol = rbac_pb2.Subject(role=request.role)
+        self.enforcer.delete_role(to_str(rol))
         return Empty()
 
     def DeleteUser(self, request, context):
-        # TODO
+        usr = rbac_pb2.Subject(user=request.user)
+        self.enforcer.delete_user(to_str(usr))
         return Empty()
 
-    def GetPermissionsForUser(self, request, context):
-        # TODO
-        return rbac_pb2.PermissionsResponse
+    def GetPermissions(self, request, context):
+        res = rbac_pb2.PermissionsResponse()
+        for it in self.enforcer.get_permissions_for_user(to_str(request)):
+            pem = permission_from_line(it)
+            res.items.extend([pem])
+        return res
 
-    def GetImplicitPermissionsForUser(self, request, context):
-        # TODO
-        return rbac_pb2.PermissionsResponse()
-
-    def GetPermissionsForRole(self, request, context):
-        # TODO
-        return rbac_pb2.PermissionsResponse()
+    def GetImplicitPermissions(self, request, context):
+        res = rbac_pb2.PermissionsResponse()
+        for it in self.enforcer.get_implicit_permissions_for_user(to_str(request)):
+            pem = permission_from_line(it)
+            res.items.extend([pem])
+        return res
 
     def DeletePermission(self, request, context):
-        # TODO
+        self.enforcer.delete_permissions_for_user(
+            to_str(request.subject), to_str(request.object), to_str(request.action))
         return Empty()
 
     def AddPermission(self, request, context):
-        # TODO
+        self.enforcer.add_permission_for_user(
+            to_str(request.subject), to_str(request.object), to_str(request.action))
         return Empty()
 
     def HasPermission(self, request, context):
-        # TODO
+        if not self.enforcer.has_permission_for_user(
+                to_str(request.subject), to_str(request.object), to_str(request.action)):
+            context.abort(StatusCode.NOT_FOUND, "")
         return Empty()
