18d6cb26df448a029d437332c6d8fd83ceff37ca
Jeremy Zheng <jitang.zheng@gmail.com>
2026-05-10 02:52:43 +0000

    :construction: add get roles/subjects/objects/actions support

--- dahlia/src/dahlia/rbac/server.py
+++ dahlia/src/dahlia/rbac/server.py
@@ -2,7 +2,7 @@ import logging
 
 from google.protobuf.empty_pb2 import Empty
 
-from dahlia.protocols import rbac_pb2_grpc, rbac_pb2
+from dahlia.protocols import rbac_pb2_grpc, rbac_pb2, from_str, to_str, role_from_str, user_from_str
 
 logger = logging.getLogger(__name__)
 
@@ -13,39 +13,47 @@ class Server(rbac_pb2_grpc.EnforcerServicer):
 
     def GetAllSubjects(self, request, context):
         res = rbac_pb2.SubjectsResponse()
-        for s in self.enforcer.get_all_subjects():
-            it = res.items.add()
-            # from_str(s, it)
+        for it in self.enforcer.get_all_subjects():
+            sub = res.items.add()
+            from_str(it, sub)
         return res
 
     def GetAllObjects(self, request, context):
         res = rbac_pb2.ObjectsResponse()
-        for s in self.enforcer.get_all_objects():
-            it = res.items.add()
-            # from_str(s, it)
+        for it in self.enforcer.get_all_objects():
+            obj = res.items.add()
+            from_str(it, obj)
         return res
 
     def GetAllActions(self, request, context):
         res = rbac_pb2.ActionsResponse()
-        for s in self.enforcer.get_all_actions():
-            it = res.items.add()
-            # from_str(s, it)
+        for it in self.enforcer.get_all_actions():
+            act = res.items.add()
+            from_str(it, act)
         return res
 
     def GetAllRoles(self, request, context):
         res = rbac_pb2.RolesResponse()
-        for s in self.enforcer.get_all_roles():
-            it = res.items.add()
-            # from_str(s, it)
+        for it in self.enforcer.get_all_roles():
+            rol = role_from_str(it, rol)
+            res.items.extend([rol])
         return res
 
     def GetRolesForUser(self, request, context):
-        # TODO
-        return rbac_pb2.RolesResponse()
+        res = rbac_pb2.RolesResponse()
+        subject = rbac_pb2.Subject(user=request)
+        for it in self.enforcer.get_roles_for_user(to_str(subject)):
+            rol = role_from_str(it)
+            res.items.extend([rol])
+        return res
 
     def GetImplicitRolesForUser(self, request, context):
-        # TODO
-        return rbac_pb2.RolesResponse()
+        res = rbac_pb2.RolesResponse()
+        subject = rbac_pb2.Subject(user=request)
+        for it in self.enforcer.get_implicit_roles_for_user(to_str(subject)):
+            rol = role_from_str(it)
+            res.items.extend([rol])
+        return res
 
     def GetUsersForRole(self, request, context):
         # TODO
