cbc834920fabe28554edd456c1eb7a26457d81e1
Jeremy Zheng <jitang.zheng@gmail.com>
2026-06-29 14:11:33 +0000

    :construction: add secret-box & password-hashing support

--- petunia/src/sodium.zig
+++ petunia/src/sodium.zig
@@ -8,16 +8,69 @@ const c = @cImport({
 });
 
 pub const Error = error{
-    LibSodiumInit,
+    LibsodiumInit,
+    LibsodiumPasswordSum,
+    LibsodiumPasswordVerify,
+    LibsodiumEncryptData,
+    LibsodiumDecryptInvalidData,
+    LibsodiumDecryptData,
 };
 
-pub fn random_bytes(buf: []u8) void {
-    c.randombytes_buf(buf.ptr, buf.len);
-}
+pub const Random = struct {
+    pub fn bytes(allocator: std.mem.Allocator, len: usize) ![]const u8 {
+        const buf = try allocator.alloc(u8, len);
+        c.randombytes_buf(buf.ptr, buf.len);
+        return buf;
+    }
+};
+
+pub const PasswordHashing = struct {
+    pub fn sum(allocator: std.mem.Allocator, password: []const u8) ![]const u8 {
+        const buf = try allocator.alloc(u8, c.crypto_pwhash_STRBYTES);
+        if (c.crypto_pwhash_str(buf.ptr, password.ptr, password.len, c.crypto_pwhash_OPSLIMIT_SENSITIVE, c.crypto_pwhash_MEMLIMIT_SENSITIVE) != 0) {
+            return Error.LibsodiumPasswordSum;
+        }
+        return buf;
+    }
+    pub fn verify(hashed: []const u8, password: []const u8) !void {
+        if (c.crypto_pwhash_str_verify(hashed.ptr, password.ptr, password.len) != c.EXIT_SUCCESS) {
+            return Error.LibsodiumPasswordVerify;
+        }
+    }
+};
+
+pub const SecretBox = struct {
+    key: []const u8,
+    pub fn init(key: []const u8) SecretBox {
+        return SecretBox{
+            .key = key,
+        };
+    }
+
+    pub fn encrypt(self: SecretBox, allocator: std.mem.Allocator, plain: []const u8) !struct { []const u8, []const u8 } {
+        const cipher = try allocator.alloc(u8, plain.len + c.crypto_secretbox_MACBYTES);
+        const nonce = try allocator.alloc(u8, c.crypto_secretbox_NONCEBYTES);
+        c.randombytes_buf(nonce.ptr, nonce.len);
+        if (c.crypto_secretbox_easy(cipher.ptr, plain.ptr, plain.len, nonce.ptr, self.key.ptr) != c.EXIT_SUCCESS) {
+            return Error.LibsodiumEncryptData;
+        }
+        return .{ cipher, nonce };
+    }
+    pub fn decrypt(self: SecretBox, allocator: std.mem.Allocator, cipher: []const u8, nonce: []const u8) ![]const u8 {
+        if (cipher.len <= c.crypto_secretbox_MACBYTES) {
+            return Error.LibsodiumDecryptInvalidData;
+        }
+        const plain = try allocator.alloc(u8, cipher.len - c.crypto_secretbox_MACBYTES);
+        if (c.crypto_secretbox_open_easy(plain.ptr, cipher.ptr, cipher.len, nonce.ptr, self.key.ptr) != c.EXIT_SUCCESS) {
+            return Error.LibsodiumDecryptData;
+        }
+        return plain;
+    }
+};
 
 pub fn init() !void {
     if (c.sodium_init() != c.EXIT_SUCCESS) {
-        return Error.LibSodiumInit;
+        return Error.LibsodiumInit;
     }
 }
 
@@ -26,19 +79,56 @@ test "libsodium random bytes" {
 
     const len = 32;
 
-    for (1..9) |i| {
-        const buf = try allocator.alloc(u8, len);
+    for (1..10) |i| {
+        const buf = try Random.bytes(allocator, len);
         defer allocator.free(buf);
-        random_bytes(buf);
+
         try std.testing.expect(buf.len == len);
         std.debug.print("random bytes({}): {x}\n", .{ i, buf });
     }
 }
 
 test "libsodium password hashing" {
-    try std.testing.expect(1 + 1 == 2);
+    const allocator = std.testing.allocator;
+    const password = "Hello, Palm!";
+    for (1..3) |i| {
+        const hashed = try PasswordHashing.sum(allocator, password);
+        defer allocator.free(hashed);
+        try std.testing.expect(hashed.len == c.crypto_pwhash_STRBYTES);
+
+        {
+            const len = std.base64.standard.Encoder.calcSize(hashed.len);
+            const str = try allocator.alloc(u8, len);
+            defer allocator.free(str);
+
+            const rst = std.base64.standard.Encoder.encode(str, hashed);
+            std.debug.print("hash password({}, {}): {s}\n", .{ i, hashed.len, rst });
+        }
+    }
 }
 
 test "libsodium encrypt data" {
-    try std.testing.expect(1 + 1 == 2);
+    const allocator = std.testing.allocator;
+    const plain = "Hello, Palm!";
+
+    const key = try allocator.alloc(u8, c.crypto_secretbox_KEYBYTES);
+    defer allocator.free(key);
+    c.crypto_secretbox_keygen(key.ptr);
+    std.debug.print("key({}), {x}\n", .{ c.crypto_secretbox_KEYBYTES, key });
+
+    const secret_box = SecretBox.init(key);
+    for (1..3) |i| {
+        const cipher = try secret_box.encrypt(allocator, plain);
+        defer allocator.free(cipher.@"0");
+        defer allocator.free(cipher.@"1");
+
+        {
+            const len = std.base64.standard.Encoder.calcSize(cipher.@"0".len);
+            const str = try allocator.alloc(u8, len);
+            defer allocator.free(str);
+
+            const rst = std.base64.standard.Encoder.encode(str, cipher.@"0");
+            std.debug.print("encrypt({}, {}): {s}\n", .{ i, cipher.@"0".len, rst });
+        }
+    }
 }
