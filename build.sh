7cb1ade29609147eae3c719a06939cb2757aeec8
Jeremy Zheng <jitang.zheng@gmail.com>
2026-07-04 06:45:05 +0000

    :hammer: add rhododendron building

--- build.sh
+++ build.sh
@@ -6,13 +6,19 @@ source /etc/os-release
 
 export WORK_DIR=$PWD
 export TARGET_DIR=$WORK_DIR/tmp
-export PACKAGE=palm-$ID-$VERSION_CODENAME-$(uname -m)-$(git describe --tags --always --dirty)
+export PACKAGE=palm-$VERSION_CODENAME-$(uname -m)-$(git describe --tags --always --dirty)
+
+if [[ "$ID" != "ubuntu" ]]; then
+    echo "Unsupported system $PRETTY_NAME"
+    exit 1
+fi
 
 # ---------------------------------------------------------
 
+# apt install libboost-all-dev
 function build_loquat() {
     local source_root=$WORK_DIR/loquat
-    local build_root=$source_root/build/Release-$ID-$VERSION_CODENAME-$(uname -m)
+    local build_root=$source_root/build/Release-$VERSION_CODENAME-$(uname -m)
     
     cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release \
         -DABSL_PROPAGATE_CXX_STD=ON \
@@ -25,9 +31,22 @@ function build_loquat() {
     cp $build_root/loquat $TARGET_DIR/$PACKAGE/bin/
 }
 
+# https://phoenix.hexdocs.pm/releases.html
+function build_rhododendron() {
+    cd $WORK_DIR/rhododendron/
+
+    mix deps.get --only prod
+    MIX_ENV=prod mix compile
+    MIX_ENV=prod mix assets.deploy
+    mix phx.gen.release
+    MIX_ENV=prod mix release
+
+    cp -r _build/prod/rel/rhododendron $TARGET_DIR/$PACKAGE/
+}
 # ---------------------------------------------------------
 
 build_loquat
+build_rhododendron
 
 XZ_OPT=-9 tar -cJf $WORK_DIR/$PACKAGE.tar.xz -C $WORK_DIR/$PACKAGE .
 
--- loquat/README.md
+++ loquat/README.md
@@ -1,7 +1,6 @@
 # Usage
 
 ```bash
-$ podman run --rm -it --events-backend=file --network host -v $(dirname $PWD):/mnt:z ubuntu:noble
-> cd /mnt/loquat/
-> ./build.sh
+cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release -DABSL_PROPAGATE_CXX_STD=ON -DTINK_USE_SYSTEM_OPENSSL=ON -DTINK_BUILD_TESTS=OFF -B build -S . -G Ninja
+cmake --build build
 ```
