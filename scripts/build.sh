1826c3fa588237c6edaf21cc42882ad0ddd69690
Jeremy Zheng <jitang.zheng@gmail.com>
2026-07-21 06:05:50 +0000

    :tada: create wisteria dashboard project

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,50 @@
 
 set -e
 
-function build_wisteria() {
-    echo "building wisteria for $1"
-    cargo build --release --target $1
-}
+source /etc/os-release
+
+export WORK_DIR=$PWD
+export PACKAGE=palm-$VERSION_CODENAME-$(uname -m)-$(git describe --tags --always --dirty)
+export TARGET_DIR=$WORK_DIR/tmp
+
 
 # ---------------------------------------------------------
 
-declare -a targets=("x86_64-unknown-linux-gnu" "aarch64-unknown-linux-gnu" "riscv64gc-unknown-linux-gnu")
+function build_wisteria() {
+    cd $WORK_DIR/
+
+    local target="$1-unknown-linux-gnu"
+    echo "building wisteria for $target"
+    cargo build --release --target $target
+
+    mkdir -p $TARGET_DIR/$PACKAGE/bin/$1
+    cp $WORK_DIR/target/$target/release/wisteria $TARGET_DIR/$PACKAGE/bin/$1/
+}
+
+# ---------------------------------------------------------
+if [ -f $TARGET_DIR/$PACKAGE.md5 ]
+then
+    echo "release $PACKAGE already exists."
+    exit 1
+fi
+
+if [ -f $TARGET_DIR/$PACKAGE.tar.xz ]
+then
+    rm $TARGET_DIR/$PACKAGE.tar.xz
+fi
+
+if [ -d $TARGET_DIR/$PACKAGE ]
+then
+    rm -r $TARGET_DIR/$PACKAGE
+fi
+
+declare -a targets=("x86_64" "aarch64" "riscv64gc")
 for t in "${targets[@]}"; do
     build_wisteria $t
 done
 
-echo "done."
+XZ_OPT=-9 tar -cJf $TARGET_DIR/$PACKAGE.tar.xz --remove-files -C $TARGET_DIR/$PACKAGE .
+md5sum $TARGET_DIR/$PACKAGE.tar.xz > $TARGET_DIR/$PACKAGE.md5
+
+echo "done($PACKAGE.tar.xz)."
 exit 0
