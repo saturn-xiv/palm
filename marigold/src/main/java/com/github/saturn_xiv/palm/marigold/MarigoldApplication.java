eab7963899a8856db415c174b59b6906844be140
Jeremy Zheng <jitang.zheng@gmail.com>
2026-07-22 11:52:29 +0000

    :tada: create marigold project

---
+++ marigold/src/main/java/com/github/saturn_xiv/palm/marigold/MarigoldApplication.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.marigold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarigoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarigoldApplication.class, args);
+	}
+
+}
---
+++ marigold/src/test/java/com/github/saturn_xiv/palm/marigold/MarigoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.marigold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarigoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
