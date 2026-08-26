2ce7b35cd1019d3860174defb8455e003abba972
Jeremy Zheng <jitang.zheng@gmail.com>
2026-08-26 14:27:28 +0000

    :tada: merge cbeta & tipitaka resources

--- docker/autumn/run.sh
+++ docker/autumn/run.sh
@@ -2,12 +2,5 @@
 
 export CODE="palm-autumn"
 
-
-if [ ! -d tmp ]
-then
-  mkdir tmp
-  chmod 777 tmp
-fi
-
-docker run --rm -it --network host -v $PWD/tmp:/srv:z $CODE
-# docker run --rm -it --network host -u $(id -u):$(id -g) -v $PWD/tmp:/srv:z $CODE
+docker run --rm -it --network host -v $PWD:/srv:z $CODE
+# docker run --rm -it --network host -u $(id -u):$(id -g) -v $PWD:/srv:z $CODE
