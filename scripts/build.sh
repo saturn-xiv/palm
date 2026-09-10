9575bf9e4587eddd3f90f471fcb8671afc7e770e
Jeremy Zheng <jitang.zheng@gmail.com>
2026-09-10 07:06:18 +0000

    :fire: remove 'v' prefix

--- scripts/build.sh
+++ scripts/build.sh
@@ -178,7 +178,7 @@ After=network-online.target
 [Service]
 Type=simple
 DynamicUser=yes
-ExecStart=/opt/amazon-corretto-26.0.2.11.1-linux-aarch64/bin/java -jar marigold-v*.jar --spring.config.name=production
+ExecStart=/opt/amazon-corretto-26.0.2.11.1-linux-aarch64/bin/java -jar marigold-*.jar --spring.config.name=production
 WorkingDirectory=/var/lib/palm/marigold
 Restart=always
 
