206b75ef860bb98a56e72db2d5da0cb59741f6ab
Jeremy Zheng <jitang.zheng@gmail.com>
2026-09-12 10:30:03 +0000

    :fire: remove base85 support

--- lavender/client/lavender.sh
+++ lavender/client/lavender.sh
@@ -10,7 +10,7 @@ function graphql_call() {
     curl -4 -X POST "$LAVENDER_HOST/graphql" -H "Content-Type: application/json; charset=utf-8" -H "Authorization: Bearer $LAVENDER_AUTH_TOKEN" -d "$1"
 }
 
-if [[ "$#" -eq 1 && "$1" == "headlamp-token" ]]; then
+if [[ "$#" -eq 1 && "$1" == "generaqte-headlamp-token" ]]; then
     graphql_call '
 {
     "query": "mutation call($id: String!, $args: [String!]!){ lavenderLaunchJob(id: $id, args: $args){createdAt} }",
@@ -21,6 +21,18 @@ elif [[ "$#" -eq 3 && "$1" == "run-deployment" ]]; then
 {
     "query": "mutation call($id: String!, $args: [String!]!){ lavenderLaunchJob(id: $id, args: $args){createdAt} }",
     "variables": {"id": "'deployment'", "args": ["'$2'", "'$3'"]}
+}'
+elif [[ "$#" -eq 1 && "$1" == "export-redis-keys" ]]; then
+    graphql_call '
+{
+    "query": "mutation call($id: String!, $args: [String!]!){ lavenderLaunchJob(id: $id, args: $args){createdAt} }",
+    "variables": {"id": "'export-redis-keys'", "args": []}
+}'
+elif [[ "$#" -eq 1 && "$1" == "export-pali-synonyms" ]]; then
+    graphql_call '
+{
+    "query": "mutation call($id: String!, $args: [String!]!){ lavenderLaunchJob(id: $id, args: $args){createdAt} }",
+    "variables": {"id": "'export-pali-synonyms'", "args": []}
 }'
 elif [[ "$#" -eq 2 && "$1" == "echo" ]]; then
     graphql_call '
@@ -35,7 +47,13 @@ elif [[ "$#" -eq 1 && "$1" == "version" ]]; then
     "variables": {}
 }'
 else
-    echo "Unsupported $@"
+    echo "USAGE:
+$0 version
+$0 echo MESSAGE
+
+$0 export-pali-synonyms
+$0 export-redis-keys
+$0 generaqte-headlamp-token DOMAIN GIT_COMMIT_ID"
     exit 1
 fi
 exit 0
