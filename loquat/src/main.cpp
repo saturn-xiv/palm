27d580df0024e47832ed65a884ee5433ecc9c09a
Jeremy Zheng <jitang.zheng@gmail.com>
2026-09-16 12:38:54 +0000

    :loud_sound: add catch std exception

--- loquat/src/main.cpp
+++ loquat/src/main.cpp
@@ -14,6 +14,8 @@ int main(int argc, char** argv) {
   loquat::Application app;
   try {
     return app.launch(argc, argv);
+  } catch (const std::exception& e) {
+    BOOST_LOG_TRIVIAL(error) << e.what();
   } catch (...) {
     spdlog::error(boost::current_exception_diagnostic_information());
   }
